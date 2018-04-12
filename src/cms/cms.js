aa2f1894a1ca030a0bff556157e47c3a7e0fc359
Philip Schönholzer <phi.sch@hotmail.ch>
2018-04-12 19:20:45 +0200

    Added testimonials to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import TestimonialPreview from './preview-templates/TestimonialPreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('testimonials', TestimonialPreview)
