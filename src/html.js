6ce229149d41ef78301a5783237100a0e763f2d1
Philip Schönholzer <phi.sch@hotmail.ch>
2018-05-19 12:18:49 +0200

    Favicon

--- src/html.js
+++ src/html.js
@@ -30,6 +30,29 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#008fd7" />
+          <meta name="msapplication-TileColor" content="#008fd7" />
+          <meta name="theme-color" content="#008fd7" />
+
           {this.props.headComponents}
           {css}
         </head>
