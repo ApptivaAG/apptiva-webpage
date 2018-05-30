8a5d0c11b235976cf8830a7a5298e2cf660d2bba
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2018-05-30 11:18:02 +0200

    changed meta tags to apptiva.ch domain

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
   title: 'Apptiva AG - iOS, Android, Desktop und Web-Applikationen',
   description:
     'Die Apptiva AG ist auf das Erstellen von kundenindividuellen Softwarelösungen spezialisiert. Damit unterstützt sie Unternehmen bei der Digitalisierung und bei der Beschleunigung ihrer Geschäftsprozesse. Die Apptiva AG setzt dazu modernste Technologien und Methodiken ein, um schlanke und passgenaue Lösungen zu liefern.',
-  url: 'https://apptiva.netlify.com',
-  logo: 'https://apptiva.netlify.com/img/logo-1200x630.png',
+  url: 'https://apptiva.ch',
+  logo: 'https://apptiva.ch/img/logo-1200x630.png',
   twitter: '@ApptivaTeam',
   fbAppID: '',
 }
