2f6146edf9977791f8ba4a8de51eb13076c254c3
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2018-07-26 16:44:13 +0200

    Updated claim

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   company: 'Apptiva AG',
   title: 'Apptiva AG - iOS, Android, Desktop und Web-Applikationen',
   description:
-    'Die Apptiva AG ist auf das Erstellen von kundenindividuellen Softwarelösungen spezialisiert. Damit unterstützt sie Unternehmen bei der Digitalisierung und bei der Beschleunigung ihrer Geschäftsprozesse. Die Apptiva AG setzt dazu modernste Technologien und Methodiken ein, um schlanke und passgenaue Lösungen zu liefern.',
+    'Die Apptiva AG ist auf das Erstellen von individuellen Softwarelösungen spezialisiert. Damit unterstützen wir Unternehmen bei der Digitalisierung und Beschleunigung ihrer Geschäftsprozesse. Die Apptiva AG setzt dazu agile, effektive und moderne Methodiken und Technologien ein.',
   url: 'https://apptiva.ch',
   logo: 'https://apptiva.ch/img/logo-1200x630.png',
   twitter: '@ApptivaTeam',
