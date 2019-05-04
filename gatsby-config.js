85db08283f0740ec7680cfcfdd77cfb42b31b38e
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2019-05-04 15:50:19 +0200

    removed a lot of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,6 @@ module.exports = {
     title: config.company,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: config.url,
-      },
-    },
     'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
@@ -38,49 +31,6 @@ module.exports = {
     'gatsby-transformer-sqip',
     {
       resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-external-links',
-          'gatsby-plugin-catch-links',
-          'gatsby-remark-smartypants',
-          {
-            resolve: 'gatsby-remark-emoji',
-            options: {
-              // default emojiConversion --> shortnameToUnicode
-              emojiConversion: 'toImage',
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 960,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-custom-blocks',
-            options: {
-              blocks: {
-                dark: { classes: 'custom-block-dark' },
-                center: { classes: 'custom-block-center' },
-                row: { classes: 'custom-block-row' },
-                col: { classes: 'custom-block-col' },
-                left: { classes: 'custom-block-left' },
-                right: { classes: 'custom-block-right' },
-                button: { classes: 'custom-block-button' },
-                avatar: { classes: 'custom-block-avatar' },
-                'no-margin': { classes: 'custom-block-no-margin' },
-              },
-            },
-          },
-          'gatsby-remark-embed-responsive-video',
-          'gatsby-remark-responsive-iframe',
-          'gatsby-remark-prismjs',
-        ],
-      },
     },
     'gatsby-plugin-svgr',
     {
