9fcf9b78a1eb6a7620c8f1d04747bd585e7ce456
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-02-25 08:53:46 +0100

    Fix fetch in IE

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,1 @@
-// const React = require('react')
-
-// // IE11 is broken without this polyfill
-// exports.onRenderBody = ({ setHeadComponents }) => {
-//   setHeadComponents([
-//     <script src="https://polyfill.io/v3/polyfill.min.js?features=fetch" />,
-//   ])
-// }
+// gatsby-ssr
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
+import fetch from 'unfetch'
+
 import { Button } from '../style'
 
 const sharedInput = css`
