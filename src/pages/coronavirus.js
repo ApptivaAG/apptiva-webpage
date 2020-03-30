24df0b12b7cfef2ac4a825fdc4fd2ef82d6f478e
Philip Schönholzer <phi.sch@hotmail.ch>
2020-03-30 10:07:04 +0200

    Fixed layout of pages without section

--- src/pages/coronavirus.js
+++ src/pages/coronavirus.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
-import { MainTitle, Container, Subtitle, Button } from '../style'
+import { MainTitle, Container, Subtitle, Button, Section } from '../style'
 import SEO from '../components/SEO'
 import config from '../config'
 import Layout from '../components/Layout'
@@ -22,63 +22,71 @@ export default () => (
     <main>
       <Helmet title={`${metadata.title} - ${config.company}`} />
       <SEO metaData={metadata} />
-      <Container>
-        <MainTitle>{metadata.title}</MainTitle>
-        <Subtitle>{metadata.description}</Subtitle>
+      <MainTitle>{metadata.title}</MainTitle>
+      <Subtitle>{metadata.description}</Subtitle>
+      <Section css="margin-top: 3em;">
+        <Container>
+          <h2>Status bei Apptiva</h2>
 
-        <h2 css="margin-bottom: 0; margin-top: 3em;">Status bei Apptiva</h2>
+          <ul>
+            <li>
+              <h3>Projekte werden normal bearbeitet</h3>
+              <br /> Alle Projekte werden normal bearbeitet. Der Coronavirus
+              hindert uns in keiner Weise an unserer Arbeit.
+            </li>
+          </ul>
+        </Container>
+      </Section>
 
-        <ul>
-          <li>
-            <h3>Projekte werden normal bearbeitet</h3>
-            <br /> Alle Projekte werden normal bearbeitet. Der Coronavirus
-            hindert uns in keiner Weise an unserer Arbeit.
-          </li>
-        </ul>
+      <Section dark>
+        <Container>
+          <h2>Unsere Massnahmen</h2>
 
-        <h2 css="margin-bottom: 0; margin-top: 3em;">Unsere Massnahmen</h2>
+          <p>
+            Damit wir auch in Zeiten des Coronavirus&apos; leistungs- und
+            reaktionsfähig bleiben, haben wir seit dem 8. März 2020
+            unterschiedliche Massnahmen ergriffen:
+          </p>
+          <ul>
+            <li>
+              <h3>Home-Office</h3>
+              <br />
+              Soweit möglich arbeiten wir zuhause. Dies tangiert uns kaum in
+              unserer Arbeit. Wir haben das Telefon umgeleitet und sind so zu
+              Bürozeiten immer noch unter 041 322 26 26 erreichbar. Wir freuen
+              uns auf ihren Anruf.
+            </li>
+            <li>
+              <h3>Remote-Meetings</h3>
+              <br />
+              Wir führen alle Meetings remote durch. Nur in Ausnahmefällen
+              treffen wir unsere Kunden in persona.
+            </li>
+            <li>
+              <h3>Befolgen der BAG-Regeln</h3>
+              <br />
+              Wir befolgen die{' '}
+              <a href="https://bag-coronavirus.ch/">
+                Verhaltens- und Hygieneregeln
+              </a>{' '}
+              des BAGs. Die entsprechenden Regeln sind bei uns in den
+              Räumlichkeiten aufgehängt.
+            </li>
+          </ul>
+        </Container>
+      </Section>
 
-        <p>
-          Damit wir auch in Zeiten des Coronavirus&apos; leistungs- und
-          reaktionsfähig bleiben, haben wir seit dem 8. März 2020
-          unterschiedliche Massnahmen ergriffen:
-        </p>
+      <Section>
+        <Container>
+          <h2>Fragen</h2>
 
-        <ul>
-          <li>
-            <h3>Home-Office</h3>
-            <br />
-            Soweit möglich arbeiten wir zuhause. Dies tangiert uns kaum in
-            unserer Arbeit. Wir haben das Telefon umgeleitet und sind so zu
-            Bürozeiten immer noch unter 041 322 26 26 erreichbar. Wir freuen uns
-            auf ihren Anruf.
-          </li>
-          <li>
-            <h3>Remote-Meetings</h3>
-            <br />
-            Wir führen alle Meetings remote durch. Nur in Ausnahmefällen treffen
-            wir unsere Kunden in persona.
-          </li>
-          <li>
-            <h3>Befolgen der BAG-Regeln</h3>
-            <br />
-            Wir befolgen die{' '}
-            <a href="https://bag-coronavirus.ch/">
-              Verhaltens- und Hygieneregeln
-            </a>{' '}
-            des BAGs. Die entsprechenden Regeln sind bei uns in den
-            Räumlichkeiten aufgehängt.
-          </li>
-        </ul>
+          <p>Bei Fragen stehen wir jederzeit zur Verfügung.</p>
 
-        <h2 css="margin-bottom: 0; margin-top: 3em;">Fragen</h2>
-
-        <p>Bei Fragen stehen wir jederzeit zur Verfügung.</p>
-
-        <Center>
-          <Button to="/#kontakt">Frage stellen</Button>
-        </Center>
-      </Container>
+          <Center>
+            <Button to="/#kontakt">Frage stellen</Button>
+          </Center>
+        </Container>
+      </Section>
     </main>
   </Layout>
 )
