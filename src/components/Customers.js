eb8f928f8ac6d756c720236bb001ed41c16cbd9a
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-01-17 12:09:20 +0100

    Added akso and st zh financedepartement

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -15,8 +15,8 @@ const ImageList = styled.div`
   justify-content: space-around;
   margin: 0 1em 1em 1em;
   > div {
-    flex: 0 1 4em;
-    margin: 0.5em 1em;
+    flex: 0 1 5em;
+    margin: 1em 1.5em;
   }
 `
 
@@ -27,25 +27,26 @@ const Customers = () => (
       <StaticImage
         src="../img/kanton-bern-200.png"
         alt="Amt für Informatik und Organisation des Kantons Bern"
-        height={50}
+        objectFit="contain"
+      />
+      <StaticImage
+        src="../img/akso.png"
+        alt="Ausgleichskasse Solothurn"
         objectFit="contain"
       />
       <StaticImage
         src="../img/die-post-logo.png"
         alt="Die Schweizerische Post AG"
-        height={45}
         objectFit="contain"
       />
       <StaticImage
         src="../img/energie360-300x72.png"
         alt="Energie 360° AG"
-        height={34}
         objectFit="contain"
       />
       <StaticImage
         src="../img/roche.png"
         alt="F. Hoffmann-La Roche AG"
-        height={50}
         objectFit="contain"
       />
       <StaticImage
@@ -53,71 +54,61 @@ const Customers = () => (
         alt="Grandcasino Bern"
         objectFit="contain"
       />
-      <StaticImage
-        src="../img/hbTec.png"
-        alt="hbTec AG"
-        height={28}
-        objectFit="contain"
-      />
+      <StaticImage src="../img/hbTec.png" alt="hbTec AG" objectFit="contain" />
       <StaticImage
         src="../img/hostettler-300x70.png"
         alt="hostettler"
-        height={34}
         objectFit="contain"
       />
       <StaticImage
         src="../img/insel-gruppe-logo-300x70.png"
         alt="Insel Gruppe"
-        height={34}
         objectFit="contain"
       />
       <StaticImage
         src="../img/luzerner-kantonsspital-500x113.png"
         alt="Luzerner Kantonsspital"
-        height={45}
         objectFit="contain"
       />
       <StaticImage
         src="../img/globus.png"
         alt="Magazine zum Globus"
-        height={38}
         objectFit="contain"
       />
       <StaticImage
         src="../img/maxonmotor-300x45.png"
         alt="maxon motor"
-        height={20}
         objectFit="contain"
       />
       <StaticImage
         src="../img/schurch-logo.png"
         alt="Schürch Getränke AG"
-        height={52}
         objectFit="contain"
       />
       <StaticImage
         src="../img/sd-295x145.png"
         alt="sd Gebäudeunterhalt AG"
-        height={50}
+        objectFit="contain"
+      />
+      <StaticImage
+        src="../img/stadt-zuerich-finanzdepartement.png"
+        alt="Stadt Zürich Finanzdepartement"
         objectFit="contain"
       />
       <StaticImage
         src="../img/suva-300x75.png"
         alt="suva"
-        height={35}
         objectFit="contain"
       />
       <StaticImage
         src="../img/vivanco-200.png"
         alt="Vivanco Gruppe AG"
-        height={60}
         objectFit="contain"
       />
 
       <StaticImage
         src="../img/welti-furrer-300x66.png"
         alt="Welti-Furrer"
-        height={28}
         objectFit="contain"
       />
     </ImageList>
