7b7d647fbb5d897f3409da82133593a9b5eb4ecb
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-08-08 11:06:05 +0200

    Changed from google analytics to plausbiel analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,25 +101,9 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-plausible`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          'UA-66015649-1', // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-        },
+        domain: `apptiva.ch`,
       },
     },
     {
