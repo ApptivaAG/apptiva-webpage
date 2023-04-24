b16c9f6acb1bc0a26b2b9cdaad8d39976f750061
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-04-24 17:48:45 +0200

    Replaced marquee

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,88 +1,176 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
-import Marquee from 'react-fast-marquee'
 
-const Customer = styled.div`
-  flex: 0 1 5em;
-  margin: 0.7em 2.5em;
-  width: 160px;
-`
-
-const Customers = () => (
+const CustomerList = () => (
   <>
-    <Marquee speed={40}>
-      <Customer>
-        <StaticImage
-          src="../img/kanton-bern-200.png"
-          alt="Amt für Informatik und Organisation des Kantons Bern"
-        />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/akso.png" alt="Ausgleichskasse Solothurn" />
-      </Customer>
-      <Customer>
-        <StaticImage
-          src="../img/die-post-logo.png"
-          alt="Die Schweizerische Post AG"
-        />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/energie360-300x72.png" alt="Energie 360° AG" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/roche.png" alt="F. Hoffmann-La Roche AG" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/grandcasino-bern.png" alt="Grandcasino Bern" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/hbTec.png" alt="hbTec AG" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/hostettler-300x70.png" alt="hostettler" />
-      </Customer>
-      <Customer>
-        <StaticImage
-          src="../img/insel-gruppe-logo-300x70.png"
-          alt="Insel Gruppe"
-        />
-      </Customer>
-      <Customer>
-        <StaticImage
-          src="../img/luzerner-kantonsspital-500x113.png"
-          alt="Luzerner Kantonsspital"
-        />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/globus.png" alt="Magazine zum Globus" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/maxonmotor-300x45.png" alt="maxon motor" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/schurch-logo.png" alt="Schürch Getränke AG" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/sd-295x145.png" alt="sd Gebäudeunterhalt AG" />
-      </Customer>
-      <Customer>
-        <StaticImage
-          src="../img/stadt-zuerich-finanzdepartement.png"
-          alt="Stadt Zürich Finanzdepartement"
-        />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/suva-300x75.png" alt="suva" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/vivanco-200.png" alt="Vivanco Gruppe AG" />
-      </Customer>
-      <Customer>
-        <StaticImage src="../img/welti-furrer-300x66.png" alt="Welti-Furrer" />
-      </Customer>
-    </Marquee>
+    <li>
+      <StaticImage
+        src="../img/kanton-bern-200.png"
+        alt="Amt für Informatik und Organisation des Kantons Bern"
+        height={70}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/akso.png"
+        alt="Ausgleichskasse Solothurn"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/die-post-logo.png"
+        alt="Die Schweizerische Post AG"
+        height={60}
+      />
+    </li>
+    <li>
+      <StaticImage src="../img/suva-300x75.png" alt="suva" height={50} />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/energie360-300x72.png"
+        alt="Energie 360° AG"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/roche.png"
+        alt="F. Hoffmann-La Roche AG"
+        height={70}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/globus.png"
+        alt="Magazine zum Globus"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/maxonmotor-300x45.png"
+        alt="maxon motor"
+        height={30}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/grandcasino-bern.png"
+        alt="Grandcasino Bern"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage src="../img/hbTec.png" alt="hbTec AG" height={40} />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/hostettler-300x70.png"
+        alt="hostettler"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/insel-gruppe-logo-300x70.png"
+        alt="Insel Gruppe"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/luzerner-kantonsspital-500x113.png"
+        alt="Luzerner Kantonsspital"
+        height={50}
+      />
+    </li>
+
+    <li>
+      <StaticImage
+        src="../img/schurch-logo.png"
+        alt="Schürch Getränke AG"
+        height={60}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/sd-295x145.png"
+        alt="sd Gebäudeunterhalt AG"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/stadt-zuerich-finanzdepartement.png"
+        alt="Stadt Zürich Finanzdepartement"
+        height={50}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/vivanco-200.png"
+        alt="Vivanco Gruppe AG"
+        height={70}
+      />
+    </li>
+    <li>
+      <StaticImage
+        src="../img/welti-furrer-300x66.png"
+        alt="Welti-Furrer"
+        height={50}
+      />
+    </li>
   </>
 )
+
+const Customers = () => (
+  <MarqueeStyle>
+    <div className="marquee">
+      <ul>
+        <CustomerList />
+      </ul>
+      <ul style={{ translate: '100%', position: 'absolute', inset: 0 }}>
+        <CustomerList />
+      </ul>
+    </div>
+  </MarqueeStyle>
+)
+
+const MarqueeStyle = styled.div`
+  overflow: hidden;
+  width: 100vw;
+  max-width: 100%;
+
+  .marquee {
+    width: clamp(220rem, 400%, 300rem);
+    margin-block: 1rem 4rem;
+    position: relative;
+  }
+  ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    animation: slide 50s linear infinite;
+    margin: 0;
+    padding: 1em 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  @keyframes slide {
+    0% {
+      transform: translate(0%);
+    }
+
+    100% {
+      transform: translate(-100%);
+    }
+  }
+`
 
 export default Customers
