b8f49380239d577037986b70821abf434422f8b4
David Decker <david.decker@apptiva.ch>
2023-09-26 09:46:49 +0200

    wrap contact and newsletter in main

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -16,37 +16,38 @@ const metadata = {
 
 const Newsletter = () => (
   <Layout>
-    <Helmet title={`Apptiva Newsletter - ${config.company}`} />
-    <SEO metaData={metadata} />
-
-    <Section>
-      <Container>
-        <MainTitle>Apptiva Newsletter</MainTitle>
-        <StaticImage
-          src="../img/mail9.png"
-          alt="Apptiva Newsletter"
-          width={960}
-        />
-      </Container>
-      <Container css="max-width: 600px;">
-        <h2 css="margin-top: 0;">
-          Bleiben Sie mit dem Apptiva Newsletter auf dem Laufenden.
-        </h2>
-        <p>
-          Den Apptiva Newsletter versenden wir <b>einmal pro Quartal</b> mit
-          News rund um die Apptiva:
-        </p>
-        <ul>
-          <li>Umgesetzte Projekte</li>
-          <li>Gewonnene Erkenntnisse</li>
-          <li>Wie wir uns laufend verbessern</li>
-        </ul>
-        <p>Hört sich gut an?</p>
-        <div css="margin-bottom: 4em;">
-          <NewsletterForm />
-        </div>
-      </Container>
-    </Section>
+    <main>
+      <Helmet title={`Apptiva Newsletter - ${config.company}`} />
+      <SEO metaData={metadata} />
+      <Section>
+        <Container>
+          <MainTitle>Apptiva Newsletter</MainTitle>
+          <StaticImage
+            src="../img/mail9.png"
+            alt="Apptiva Newsletter"
+            width={960}
+          />
+        </Container>
+        <Container css="max-width: 600px;">
+          <h2 css="margin-top: 0;">
+            Bleiben Sie mit dem Apptiva Newsletter auf dem Laufenden.
+          </h2>
+          <p>
+            Den Apptiva Newsletter versenden wir <b>einmal pro Quartal</b> mit
+            News rund um die Apptiva:
+          </p>
+          <ul>
+            <li>Umgesetzte Projekte</li>
+            <li>Gewonnene Erkenntnisse</li>
+            <li>Wie wir uns laufend verbessern</li>
+          </ul>
+          <p>Hört sich gut an?</p>
+          <div css="margin-bottom: 4em;">
+            <NewsletterForm />
+          </div>
+        </Container>
+      </Section>
+    </main>
   </Layout>
 )
 export default Newsletter
