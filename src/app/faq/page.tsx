e0e2e6ddce2704f5a8859a6107510bd0ab16b196
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-01-10 16:30:43 +0100

    Added page to link to other pages

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -4,19 +4,19 @@ import { SanityDocument } from 'next-sanity'
 import { faqsQuery } from '@/sanity/lib/queries'
 import { sanityFetch, token } from '@/sanity/lib/sanityFetch'
 import { draftMode } from 'next/headers'
-import Link from 'next/link'
 
 export default async function Home() {
+  const faqs = await sanityFetch<SanityDocument[]>({ query: faqsQuery })
+
   return (
     <div className="container mx-auto px-4">
-      <h1>Apptiva</h1>
+      <h1>FAQ</h1>
       <ul>
-        <li>
-          <Link href="blog">Blog</Link>
-        </li>
-        <li>
-          <Link href="faq">FAQ</Link>
-        </li>
+        {faqs.map((faq) => (
+          <li key={faq._id}>
+            <a href={faq.slug}>{faq.question}</a>
+          </li>
+        ))}
       </ul>
     </div>
   )
