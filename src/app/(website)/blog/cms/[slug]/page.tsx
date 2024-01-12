72c113de3d45234965a62d32e25541f379eb2db8
Carla Iten <carla.iten@apptiva.ch>
2024-01-12 15:36:14 +0100

    updated queries and page for blog entries from sanity

--- src/app/(website)/blog/cms/[slug]/page.tsx
+++ src/app/(website)/blog/cms/[slug]/page.tsx
@@ -3,14 +3,40 @@
 import { blogBySlugQuery } from '@/sanity/lib/queries'
 import { sanityFetch } from '@/sanity/lib/sanityFetch'
 import { SanityDocument } from 'next-sanity'
+import Image from 'next/image'
 
 export default async function Home(props: { params: { slug: string } }) {
-const blog = await sanityFetch<SanityDocument[]>({ query: blogBySlugQuery(props.params.slug) })
+  const blog = await sanityFetch<SanityDocument[]>({
+    query: blogBySlugQuery(props.params.slug),
+  })
+
+  /*
+  todo: add 
+    header
+    creation at *
+    modified at *
+    image
+
+    image caption
+    author (ref person)
+    tags
+  */
 
   return (
     <div className="container mx-auto px-4">
-      <h1>Hoi {props.params.slug}</h1>      
-      {blog.length}      
+      <h1>Hoi {props.params.slug}</h1>
+      {blog.map((b) => (
+        <>
+          <div>
+            erstellt: {b._createdAt}, letzte änderung: {b._updatedAt}
+          </div>
+          <br />
+          <div>{b.header.title}</div>
+          <div>{b.header.description}</div>
+
+          {/*{b.image.asset._ref}<Image src={b.image.ref} alt={'tschese'}></Image>*/}
+        </>
+      ))}
     </div>
   )
 }
--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -12,17 +12,18 @@ export const faqQuery = groq`*[_type == "faq" && slug.current == $slug][0]{
     question, answer, slug, tags
   }`
 
-
-
 // Get all blog Entries
 export const blogsQuery = groq`*[_type == "blog"] {
   _id, slug, header{title}, tags
 }`
 
 // Get blog Entry by Slug
-export const blogBySlugQuery = (slug: string) => groq`*[_type == "blog" && slug.current == "${slug}"]
+export const blogBySlugQuery = (
+  slug: string
+) => groq`*[_type == "blog" && slug.current == "${slug}"]
 {
   _createdAt,_updatedAt,  
-  header{title, description} 
-
+  header{title, description, image, content },
+  modules,
+  author
 }`
