26477cc52c881675ca55fe1be2630e6d14ec8a0b
Markus Tanner <markus.tanner@apptiva.ch>
2024-01-15 13:26:10 +0100

    using persons as document

--- src/sanity/schema.ts
+++ src/sanity/schema.ts
@@ -7,12 +7,25 @@ import servicePage from './schemas/documents/service-page'
 import tag from './schemas/documents/tag'
 import header from './schemas/objects/header'
 import module from './schemas/objects/module'
-import person from './schemas/objects/person'
+import person from './schemas/documents/person'
 import social from './schemas/objects/social'
 import contact from './schemas/objects/contact'
 import skill from './schemas/objects/skill'
 import skillItem from './schemas/objects/skill-item'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [servicePage, faq, glossary, header, module, tag, blog, person, social, contact, skill, skillItem],
+  types: [
+    servicePage,
+    faq,
+    glossary,
+    header,
+    module,
+    tag,
+    blog,
+    person,
+    social,
+    contact,
+    skill,
+    skillItem,
+  ],
 }
