33f6a105bd478a5463902d23bea6a6c35b4d13e4
Markus Tanner <markus.tanner@apptiva.ch>
2024-01-15 16:24:14 +0100

    projects added to cms

--- src/sanity/schema.ts
+++ src/sanity/schema.ts
@@ -3,6 +3,7 @@ import { type SchemaTypeDefinition } from 'sanity'
 import blog from './schemas/documents/blog'
 import faq from './schemas/documents/faq'
 import glossary from './schemas/documents/glossary'
+import project from './schemas/documents/project'
 import servicePage from './schemas/documents/service-page'
 import tag from './schemas/documents/tag'
 import header from './schemas/objects/header'
@@ -18,6 +19,7 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     servicePage,
     faq,
     glossary,
+    project,
     header,
     module,
     tag,
