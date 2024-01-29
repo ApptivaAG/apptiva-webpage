bd1d86e7a8b9b6264d42ad26b2d521df5c742c0c
David Decker <david.decker@apptiva.ch>
2024-01-29 11:40:56 +0100

    clean up unused queries

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,47 +1,6 @@
 // ./nextjs-app/sanity/lib/queries.ts
 
 import { nullToUndefined, q, sanityImage } from 'groqd'
-import { groq } from 'next-sanity'
-
-// Get all posts
-export const faqsQuery = groq`*[_type == "faq"]{
-    _id, question, answer, slug, tags
-  }`
-
-// Get a single post by its slug
-export const faqQuery = groq`*[_type == "faq" && slug.current == $slug][0]{ 
-    question, answer, slug, tags
-  }`
-
-// Get all blog Entries
-export const blogsQuery = groq`*[_type == "blog"] {
-  _id, slug, header{title}, tags
-}`
-
-// Get all service pages
-export const servicePagesQuery = groq`*[_type == "service-page"] {
-  _id, slug, header{title}, tags
-}`
-
-// Get blog Entry by Slug
-export const blogBySlugQuery = (
-  slug: string
-) => groq`*[_type == "blog" && slug.current == "${slug}"]
-{
-  _createdAt,_updatedAt,  
-  header{title, description, image, content },
-  modules,
-  author
-}`
-
-export const servicePageBySlugQuery = (
-  slug: string
-) => groq`*[_type == "service-page" && slug.current == "${slug}"]
-{
-  _createdAt,_updatedAt,  
-  header{title, description, image, content},
-  modules{title, layout, image, content},
-}`
 
 export const queryPostsFromCms = q('*')
   .filterByType('blog')
