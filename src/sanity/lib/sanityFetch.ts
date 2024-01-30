7e7001003716cbc41716d5e3e42759a844d0ab8d
David Decker <david.decker@apptiva.ch>
2024-01-30 14:02:20 +0100

    include tags in sanity fetch

--- src/sanity/lib/sanityFetch.ts
+++ src/sanity/lib/sanityFetch.ts
@@ -1,12 +1,23 @@
 import 'server-only'
 
-import { client } from '@/sanity/lib/client'
 import { makeSafeQueryRunner } from 'groqd'
+import { createClient } from 'next-sanity'
 
 export const token = process.env.SANITY_API_READ_TOKEN
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-08'
+
+const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
+})
+
 export const runQuery = makeSafeQueryRunner(
-  (query, params: Record<string, number | string> = {}) => {
+  (query, params: Record<string, number | string> = {}, tags?: string[]) => {
     const isDraftMode = false
 
     if (isDraftMode && !token) {
@@ -15,6 +26,8 @@ export const runQuery = makeSafeQueryRunner(
       )
     }
 
-    return client.withConfig({ useCdn: !isDraftMode }).fetch(query, params)
+    return client
+      .withConfig({ useCdn: !isDraftMode })
+      .fetch(query, params, { next: { tags } })
   }
 )
