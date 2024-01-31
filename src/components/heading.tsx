139b4526a3c3ca23b905aa3331f61c6a120f18a0
David Decker <david.decker@apptiva.ch>
2024-01-31 16:54:12 +0100

    add hero and heading

---
+++ src/components/heading.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type Level = 1 | 2 | 3 | 4
+
+const getClassNameByLevel = (level: Level): string => {
+  switch (level) {
+    case 1:
+      return 'text-[7.5rem] font-[900] leading-[6.25rem]'
+    case 2:
+      return 'text-[4rem] font-[600] leading-[4rem]'
+    case 3:
+      return 'text-[3rem] font-[600] leading-[3.125rem]'
+    case 4:
+      return 'text-[2rem] font-[600] leading-[2.5rem]'
+    default:
+      return ''
+  }
+}
+
+export default function Heading(props: {
+  level: Level
+  size?: Level
+  className?: string
+  children: React.ReactNode
+}) {
+  return React.createElement(
+    `h${props.level}`,
+    {
+      className: twMerge(
+        getClassNameByLevel(props.size ?? props.level),
+        props.className
+      ),
+    },
+    props.children
+  )
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,16 @@
+import Heading from './heading'
+
+const Hero = () => {
+  return (
+    <div className="full flex h-[52rem] items-center justify-center bg-primary">
+      <Heading level={1} className="p-8">
+        <span className="text-base-white">Hi, wir sind</span>{' '}
+        <span className="mt-[1.25rem] inline-block border-b-[20px] border-b-primary-dark leading-[4.75rem] text-secondary">
+          apptiva
+        </span>
+      </Heading>
+    </div>
+  )
+}
+
+export default Hero
