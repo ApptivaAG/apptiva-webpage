7e1617db99c89b48fea69c550049482b2198ddb1
Kevin Rickenbach <kevin.rickenbach@apptiva.ch>
2024-02-01 17:39:08 +0100

    load testimonial images and first styling

--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -1,17 +1,30 @@
-const Testimonial = ({ testimonial }: { testimonial: any }) => {
-    return (
-        <>
-            <div>
-                {testimonial.statement}
-            </div>
-            <div>
-                <p>{testimonial.name}</p>
-                <p>{testimonial.position}</p>
-                <p>{testimonial.company}</p>
-            </div>
-        </>
+import Image from 'next/image'
+import path from 'path'
 
-    )
+const Testimonial = ({ testimonial }: { testimonial: any }) => {
+  const imagePath = path.join('/img/testimonials/', testimonial.avatar)
+  return (
+    <div>
+      <div>
+        <p className="text-4xl text-primary-dark">{testimonial.statement}</p>
+      </div>
+      <div className="flex items-center gap-8">
+        <div className="">
+          <Image
+            className="rounded-lg"
+            src={imagePath}
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="text-lg">
+          <p>{testimonial.name}</p>
+          <p>{testimonial.position}</p>
+          <p>{testimonial.company}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Testimonial
--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,15 +1,9 @@
-import Heading from './heading';
-import Testimonial from './testimonial';
+import Testimonial from './testimonial'
 
 const Testimonials = ({ testimonials }: { testimonials: any }) => {
-  console.log("testimonails client",testimonials);
-  console.log("länge", testimonials.length);
-  
   return (
     <div>
-      <Heading level={2}>Testimonials</Heading>
-
-      {testimonials.map((testimonial:any)=> {
+      {testimonials.map((testimonial: any) => {
         return <Testimonial testimonial={testimonial}></Testimonial>
       })}
     </div>
