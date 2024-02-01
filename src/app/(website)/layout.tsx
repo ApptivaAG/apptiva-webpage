cc4aa2e82e77eafc47f6e515064567dea7dab63c
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-02-01 09:25:22 +0100

    Cleaned up root layout

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 import Navbar from './../../components/Navbar'
 import './globals.css'
@@ -23,74 +22,6 @@ const gentona = localFont({
     },
   ],
 })
-
-const navbarData = [
-  {
-    title: 'Angebot',
-    href: '/angebot/',
-    type: 'one',
-    items: [
-      {
-        title: 'Development',
-        text: '',
-        href: '/development/',
-      },
-      {
-        title: 'Chatbot',
-        text: '',
-        href: '/chatbot/',
-      },
-      {
-        title: 'Consulting',
-        text: '',
-        href: '/consulting/',
-      },
-    ],
-  },
-  { title: 'Projekte', href: '/projekte/' },
-  {
-    title: 'Apptiva',
-    href: '/ueber-uns/',
-    type: 'two',
-    items: [
-      {
-        title: 'Kultur & Firmenfacts',
-        text: 'Mehr über uns erfahren',
-        href: '/ueber-uns/',
-      },
-      {
-        title: 'Team',
-        text: 'Unser erfahrenes Entwicklungsteam',
-        href: '/ueber-uns/#team',
-      },
-      {
-        title: 'Kompetenzen',
-        text: 'Dies sind unsere Kompetenzen',
-        href: '/unsere-kompetenzen/',
-      },
-      {
-        title: 'Jobs',
-        text: 'Jetzt bewerben',
-        href: '/jobs/',
-      },
-    ],
-  },
-  { title: 'Kontakt', href: '/kontakt/' },
-  { title: 'Blog', href: '/blog/' },
-  {
-    title: 'Wissen',
-    href: '/wissen/',
-    items: [
-      {
-        title: 'FAQ',
-        href: '/faq/',
-      },
-    ],
-  },
-  { title: 'CMS', href: '/studio/' },
-]
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://apptiva.ch'),
