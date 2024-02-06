315aca2c6e2bbbde6ef40f608b48229c1e179c40
Kevin Rickenbach <kevin.rickenbach@apptiva.ch>
2024-02-06 13:55:01 +0100

    moved testimonials code

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,11 +1,11 @@
 // ./nextjs-app/app/page.tsx
-import Hero from '@/components/hero';
-import Testimonials from '@/components/testimonials';
-import { getTestimonialsData } from '../../../lib/testimonials';
+import Hero from '@/components/hero'
+import Testimonials from '@/components/testimonials'
+import { getTestimonialsData } from '../../utils/testimonials'
 
 export default async function Home() {
   const testimonials = getTestimonialsData()
-  
+
   return (
     <>
       <Hero />
