7d65f1adad6b28745c352dadb8ae1124dfb3e71a
David Decker <david.decker@apptiva.ch>
2024-02-06 11:02:09 +0100

    display nav subitems

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import apptivaLogo from './logo.svg'
 import Image from 'next/image'
 import Button from './button'
+import { useState } from 'react'
 
 const navbarData = [
   {
@@ -57,43 +60,58 @@ const navbarData = [
   { title: 'Blog', href: '/blog/' },
 ]
 
-const Navbar = () => (
-  <div className="fixed w-full p-4">
-    <div className="flex items-center justify-between rounded-md bg-[#053A78dd] px-8 py-6 text-base-white backdrop-blur-[6px]">
-      <Link href={'/'}>
-        <Image priority src={apptivaLogo} alt="Apptiva Logo" className="w-32" />
-      </Link>
-      <nav className="flex justify-between gap-8 rounded bg-black/40 px-8 py-3">
-        {navbarData.map((item) => (
-          <div key={item.href}>
-            <ul key={item.href}>
-              <li key={item.href}>
-                <Link key={item.href} href={item.href} className="">
-                  {item.title}
-                </Link>
-                {/* {item.items && (
-                <div>
-                  <ul>
-                    {item.items.map((subitem) => (
-                      <li key={subitem.href}>
-                        <Link href={subitem.href}>{subitem.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )} */}
-              </li>
-            </ul>
-          </div>
-        ))}
-      </nav>
-      <Link href={'/kontakt/'}>
-        <Button element="div" intent="secondary">
-          Kontakt
-        </Button>
-      </Link>
+const Navbar = () => {
+  const [activeNavigationItem, setActiveNavigationItem] = useState<
+    string | undefined
+  >(undefined)
+
+  return (
+    <div className="fixed w-full p-4">
+      <div className="flex items-center justify-between rounded-md bg-[#053A78dd] px-8 py-6 text-base-white backdrop-blur-[6px]">
+        <Link href={'/'}>
+          <Image
+            priority
+            src={apptivaLogo}
+            alt="Apptiva Logo"
+            className="w-32"
+          />
+        </Link>
+        <nav className="flex justify-between gap-8 rounded bg-black/40 px-8 py-3">
+          {navbarData.map((item) => (
+            <div
+              key={item.href}
+              onMouseOver={() => setActiveNavigationItem(item.href)}
+              onMouseOut={() => setActiveNavigationItem(undefined)}
+            >
+              <ul key={item.href}>
+                <li key={item.href} className="relative">
+                  <Link key={item.href} href={item.href}>
+                    {item.title}
+                  </Link>
+                  {activeNavigationItem === item.href && item.items && (
+                    <div className="absolute left-[-1rem] rounded bg-primary-dark">
+                      <ul className="rounded bg-black/40 p-4">
+                        {item.items.map((subitem) => (
+                          <li key={subitem.href}>
+                            <Link href={subitem.href}>{subitem.title}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </li>
+              </ul>
+            </div>
+          ))}
+        </nav>
+        <Link href={'/kontakt/'}>
+          <Button element="div" intent="secondary">
+            Kontakt
+          </Button>
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Navbar
