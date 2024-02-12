f7d7796f02c1f8ec5aa378e6086e2507ef49a2c5
David Decker <david.decker@apptiva.ch>
2024-02-12 15:50:16 +0100

    sort customers

--- src/utils/customers.ts
+++ src/utils/customers.ts
@@ -3,17 +3,6 @@ import path from 'path'
 
 const customerList = [
   {
-    fileName: 'kanton-bern-200.png',
-    alt: 'Amt für Informatik und Organisation des Kantons Bern',
-    height: 50,
-  },
-
-  {
-    fileName: 'akso.png',
-    alt: 'Ausgleichskasse Solothurn',
-    height: 35,
-  },
-  {
     fileName: 'post-logo-2023.png',
     alt: 'Die Schweizerische Post AG',
     height: 55,
@@ -24,13 +13,13 @@ const customerList = [
     height: 35,
   },
   {
-    fileName: 'energie360-300x72.png',
-    alt: 'Energie 360° AG',
-    height: 35,
-  },
-  {
     fileName: 'roche.png',
     alt: 'F. Hoffmann-La Roche AG',
+    height: 50,
+  },
+  {
+    fileName: 'vivanco-200.png',
+    alt: 'Vivanco Gruppe AG',
     height: 50,
   },
   {
@@ -39,9 +28,34 @@ const customerList = [
     height: 35,
   },
   {
-    fileName: 'maxonmotor-300x45.png',
-    alt: 'maxon motor',
-    height: 20,
+    fileName: 'kanton-bern-200.png',
+    alt: 'Amt für Informatik und Organisation des Kantons Bern',
+    height: 50,
+  },
+  {
+    fileName: 'stadt-zuerich-finanzdepartement.png',
+    alt: 'Stadt Zürich Finanzdepartement',
+    height: 35,
+  },
+  {
+    fileName: 'luzerner-kantonsspital-500x113.png',
+    alt: 'Luzerner Kantonsspital',
+    height: 35,
+  },
+  {
+    fileName: 'akso.png',
+    alt: 'Ausgleichskasse Solothurn',
+    height: 35,
+  },
+  {
+    fileName: 'fhnw-logo.png',
+    alt: 'Fachhochschule Nordwestschweiz',
+    height: 45,
+  },
+  {
+    fileName: 'screenimage-logo.png',
+    alt: 'Screenimage Systems AG',
+    height: 40,
   },
   {
     fileName: 'grandcasino-bern.png',
@@ -49,9 +63,9 @@ const customerList = [
     height: 35,
   },
   {
-    fileName: 'hbTec.png',
-    alt: 'hbTec AG',
-    height: 22,
+    fileName: 'energie360-300x72.png',
+    alt: 'Energie 360° AG',
+    height: 35,
   },
   {
     fileName: 'hostettler-300x70.png',
@@ -59,13 +73,18 @@ const customerList = [
     height: 35,
   },
   {
-    fileName: 'insel-gruppe-logo-300x70.png',
-    alt: 'Insel Gruppe',
-    height: 35,
+    fileName: 'maxonmotor-300x45.png',
+    alt: 'maxon motor',
+    height: 20,
   },
   {
-    fileName: 'luzerner-kantonsspital-500x113.png',
-    alt: 'Luzerner Kantonsspital',
+    fileName: 'hbTec.png',
+    alt: 'hbTec AG',
+    height: 22,
+  },
+  {
+    fileName: 'insel-gruppe-logo-300x70.png',
+    alt: 'Insel Gruppe',
     height: 35,
   },
   {
@@ -79,33 +98,13 @@ const customerList = [
     height: 35,
   },
   {
-    fileName: 'stadt-zuerich-finanzdepartement.png',
-    alt: 'Stadt Zürich Finanzdepartement',
-    height: 35,
-  },
-  {
-    fileName: 'vivanco-200.png',
-    alt: 'Vivanco Gruppe AG',
-    height: 50,
-  },
-  {
     fileName: 'welti-furrer-300x66.png',
     alt: 'Welti-Furrer',
     height: 25,
   },
   {
-    fileName: 'fhnw-logo.png',
-    alt: 'Fachhochschule Nordwestschweiz',
-    height: 45,
-  },
-  {
     fileName: 'baumgartner-logo.png',
     alt: 'Baumgartner Fenster',
-    height: 40,
-  },
-  {
-    fileName: 'screenimage-logo.png',
-    alt: 'Screenimage Systems AG',
     height: 40,
   },
 ]
