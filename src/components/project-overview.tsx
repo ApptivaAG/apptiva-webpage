f7c6e5300a53fb66d1d75143cc839120075df3bb
Kevin Rickenbach <kevin.rickenbach@apptiva.ch>
2024-02-13 15:43:40 +0100

    load project image for preview

--- src/components/project-overview.tsx
+++ src/components/project-overview.tsx
@@ -1,8 +1,11 @@
+import SanityImage from './sanity-image'
+
 const ProjectOverview = ({ project }: { project: any }) => {
   return (
     <>
       <div>
         <div className="m-auto w-8/12 ">
+          <SanityImage image={project.image} />
           <h3>{project.projectName}</h3>
         </div>
       </div>
