9bd8444777670fdabd662e84ed178d76e9a6482a
Carla Iten <carla.iten@apptiva.ch>
2024-02-14 15:36:09 +0100

    added VisualEditing component and used it in main layout if draftMode is enabled

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,4 +1,6 @@
+import LiveVisualEditing from '@/components/live-visual-editing'
 import type { Metadata } from 'next'
+import { draftMode } from 'next/headers'
 
 export const metadata: Metadata = {
   title: 'CMS Apptiva Website',
@@ -12,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {draftMode().isEnabled && <LiveVisualEditing />}
+      </body>
     </html>
   )
 }
