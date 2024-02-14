a62034adacc4c3b9afe838cece15e8292dc1a3fb
Carla Iten <carla.iten@apptiva.ch>
2024-02-14 17:23:39 +0100

    added presentation tool to sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { groqdPlaygroundTool } from 'groqd-playground'
 import { debugSecrets } from '@sanity/preview-url-secret/sanity-plugin-debug-secrets'
+import { presentationTool } from 'sanity/presentation'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './src/sanity/env'
@@ -25,5 +26,12 @@ export default defineConfig({
     visionTool({ defaultApiVersion: apiVersion }),
     groqdPlaygroundTool(),
     debugSecrets(),
+    presentationTool({
+      previewUrl: {
+        draftMode: {
+          enable: '/api/draft',
+        },
+      },
+    }),
   ],
 })
