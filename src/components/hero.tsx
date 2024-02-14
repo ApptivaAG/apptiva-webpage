725af2b9b371a4dccf4cb8179e41ca19f1b07fea
David Decker <david.decker@apptiva.ch>
2024-02-14 14:20:39 +0100

    add gradient background to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import Heading from './heading'
 
 export default function Hero() {
   return (
-    <div className="full flex h-[52rem] items-center justify-center bg-primary">
+    <div className="full flex h-[52rem] items-center justify-center bg-gradient-to-br from-primary to-primary-dark">
       <Heading level={1} className="p-8">
         <span className="text-base-white">Hi, wir sind</span>{' '}
         <span className="mt-[1.25rem] inline-block border-b-[20px] border-b-primary-dark leading-[4.75rem] text-secondary">
