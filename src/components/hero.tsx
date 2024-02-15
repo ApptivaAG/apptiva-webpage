8e4df03f358521aafd97706d59f2baf85f1c72a0
David Decker <david.decker@apptiva.ch>
2024-02-15 11:46:34 +0100

    additional styling for hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,12 +1,15 @@
 import { CmsContent } from '@/utils/types'
-import Heading from './heading'
 import StyledPortableText from './styled-portable-text'
 
 export default function Hero({ claim }: { claim: CmsContent }) {
   return (
-    <div className="full animate-gradient bg-300% from-primary-light min-h-[52rem] items-center bg-gradient-to-br to-primary-dark px-[9rem] py-[11rem] text-base-white">
+    <div className="full min-h-[52rem] animate-gradient items-center bg-gradient-to-br from-primary-light to-primary-dark bg-300% px-[9rem] py-[11rem] text-base-white">
       {claim?.map((content) => (
-        <StyledPortableText key={content._key} content={content} />
+        <StyledPortableText
+          key={content._key}
+          content={content}
+          spreadParagraphs
+        />
       ))}
     </div>
   )
--- src/components/styled-portable-text.tsx
+++ src/components/styled-portable-text.tsx
@@ -2,26 +2,37 @@ import { PortableTextBlock } from '@/utils/types'
 import { PortableText, PortableTextComponents } from '@portabletext/react'
 import Heading from './heading'
 
-const components: PortableTextComponents = {
-  marks: {
-    // Ex. 1: custom renderer for the em / italics decorator
-    underline: ({ children }) => (
-      <span className="highlighted-text">{children}</span>
-    ),
-    strong: ({ children }) => (
-      <span className="highlighted-text">{children}</span>
-    ),
-  },
-  block: {
-    h1: ({ children }) => <Heading level={1}>{children}</Heading>,
-    h2: ({ children }) => <Heading level={2}>{children}</Heading>,
-    h3: ({ children }) => <Heading level={3}>{children}</Heading>,
-    h4: ({ children }) => <Heading level={4}>{children}</Heading>,
-  },
-}
+const StyledPortableText = ({
+  content,
+  spreadParagraphs = false,
+}: {
+  content: PortableTextBlock
+  spreadParagraphs?: boolean
+}) => {
+  const components: PortableTextComponents = {
+    marks: {
+      // Ex. 1: custom renderer for the em / italics decorator
+      underline: ({ children }) => (
+        <span className="highlighted-text">{children}</span>
+      ),
+      strong: ({ children }) => (
+        <span className="highlighted-text">{children}</span>
+      ),
+    },
+    block: {
+      h1: ({ children }) => <Heading level={1}>{children}</Heading>,
+      h2: ({ children }) => <Heading level={2}>{children}</Heading>,
+      h3: ({ children }) => <Heading level={3}>{children}</Heading>,
+      h4: ({ children }) => <Heading level={4}>{children}</Heading>,
+      ...(spreadParagraphs && {
+        normal: ({ children }) => <p className="py-4">{children}</p>,
+      }),
+    },
+  }
 
-const StyledPortableText = ({ content }: { content: PortableTextBlock }) => (
-  <PortableText key={content._key} value={content} components={components} />
-)
+  return (
+    <PortableText key={content._key} value={content} components={components} />
+  )
+}
 
 export default StyledPortableText
