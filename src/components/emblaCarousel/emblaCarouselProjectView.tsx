cb69f0d2e0f99c1bbd0b117188a155b8a84060f6
David Decker <david.decker@apptiva.ch>
2024-02-15 11:23:53 +0100

    fix project overview key error

--- src/components/emblaCarousel/emblaCarouselProjectView.tsx
+++ src/components/emblaCarousel/emblaCarouselProjectView.tsx
@@ -10,17 +10,15 @@ const EmblaCarouselProjectView: React.FC<PropType> = (props) => {
   const { slides } = props
 
   return (
-    <>
-      <EmblaCarousel slides={slides}>
-        {slides.map((slide: any) => {
-          return (
-            <div key={slide.id} className="mb-20 mt-20 min-w-0 flex-[0_0_100%]">
-              <ProjectOverview project={slide}></ProjectOverview>
-            </div>
-          )
-        })}
-      </EmblaCarousel>
-    </>
+    <EmblaCarousel slides={slides}>
+      {slides.map((slide: any) => {
+        return (
+          <div key={slide._id} className="mb-20 mt-20 min-w-0 flex-[0_0_100%]">
+            <ProjectOverview project={slide}></ProjectOverview>
+          </div>
+        )
+      })}
+    </EmblaCarousel>
   )
 }
 
