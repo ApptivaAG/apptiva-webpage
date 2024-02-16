94922a7bb58e0722b4deb437301f6e60334eed97
Carla Iten <carla.iten@apptiva.ch>
2024-02-16 18:31:48 +0100

    added style to service teasers

--- src/components/service-teaser.tsx
+++ src/components/service-teaser.tsx
@@ -1,25 +1,50 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  PromiseLikeOfReactNode,
-} from 'react'
-import Heading from './heading'
 import { PortableText } from '@portabletext/react'
+import Heading from './heading'
 import SanityImage from './sanity-image'
 
-const ServiceTeaser = ({ service }: { service: any }) => {
+const ServiceTeaser = ({
+  service,
+  rowNumber,
+}: {
+  service: any
+  rowNumber: number
+}) => {
   console.log('servcies', service)
 
-  return (
-    <div className="full m-4">
-      <div className="border-fuchsia-400  border-2">
-        <Heading level={2}>{service.header.title}</Heading>
-        <SanityImage image={service.illustration} />
+  const text = (
+    <div className="w-1/2 ">
+      <div className="m-28 text-primary">
+        <Heading level={2} size={3} className="pb-6">
+          {service.header.title}
+        </Heading>
         {service.teaser?.map((content: any) => (
           <PortableText key={content._key} value={content} />
         ))}
+      </div>
+    </div>
+  )
+
+  const image = (
+    <div className="w-1/2 rounded-md  bg-primary">
+      <div className="h-[99%] w-[99%] ">
+        {/* <SanityImage image={service.illustration} /> */}
+      </div>
+    </div>
+  )
+  return (
+    <div className="full">
+      <div className="m-auto flex rounded-md p-2 pt-2">
+        {rowNumber % 2 ? (
+          <div className="flex rounded-md bg-base-grey">
+            {image}
+            {text}
+          </div>
+        ) : (
+          <div className="flex  rounded-md bg-base-grey">
+            {text}
+            {image}
+          </div>
+        )}
       </div>
     </div>
   )
--- src/components/service-teasers.tsx
+++ src/components/service-teasers.tsx
@@ -1,11 +1,3 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  PromiseLikeOfReactNode,
-} from 'react'
-import Heading from './heading'
 import ServiceTeaser from './service-teaser'
 
 const ServiceTeasers = ({ services }: { services: any[] }) => {
@@ -13,13 +5,9 @@ const ServiceTeasers = ({ services }: { services: any[] }) => {
 
   return (
     <div className="full ">
-      <div className="m-4 ">
-        <div className="m-auto w-8/12 font-[600]">
-          {services.map((service: any) => (
-            <ServiceTeaser service={service} />
-          ))}
-        </div>
-      </div>
+      {services.map((service: any, index: number) => (
+        <ServiceTeaser service={service} rowNumber={index} />
+      ))}
     </div>
   )
 }
