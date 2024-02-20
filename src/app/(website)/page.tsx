fdf9509a74d5115fcc553666557230e2438aa644
Carla Iten <carla.iten@apptiva.ch>
2024-02-20 17:46:19 +0100

    temporarily remove service teasers

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -2,13 +2,12 @@
 import Customers from '@/components/customers'
 import Hero from '@/components/hero'
 import Projects from '@/components/projects'
+import ServiceTeasers from '@/components/service-teasers'
 import Testimonials from '@/components/testimonials'
 import getCustomerLogos from '@/utils/customers'
 import { getProjects } from '@/utils/project'
-import { getTestimonialsData } from '../../utils/testimonials'
 import { getSettings } from '@/utils/settings'
-import ServiceTeasers from '@/components/service-teasers'
-import { getServiceTeasers } from '@/utils/service-teasers'
+import { getTestimonialsData } from '../../utils/testimonials'
 
 export default async function Home() {
   const projects = await getProjects()
@@ -19,12 +18,10 @@ export default async function Home() {
   return (
     <>
       <Hero claim={claim} />
-      <ServiceTeasers />
+      {/* <ServiceTeasers /> */}
       <Projects projects={projects}></Projects>
       <Testimonials testimonials={testimonials} />
       <Customers customers={customers} />
     </>
   )
 }
-
-
