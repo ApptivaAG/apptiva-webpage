de3c8d891426e26936fbe6f69f8b29a4f786754a
Carla Iten <carla.iten@apptiva.ch>
2024-02-20 09:07:39 +0100

    increased size of feature, used it in teaser

--- src/components/service-teasers.tsx
+++ src/components/service-teasers.tsx
@@ -4,7 +4,7 @@ const ServiceTeasers = ({ services }: { services: any[] }) => {
   console.log('servcies', services)
 
   return (
-    <div className="full space-y-4 p-4 [&>*:nth-child(even)]:flex-row-reverse ">
+    <div className="feature space-y-4 p-4 [&>*:nth-child(even)]:flex-row-reverse ">
       {services.map((service: any, index: number) => (
         <ServiceTeaser service={service} rowNumber={index} />
       ))}
