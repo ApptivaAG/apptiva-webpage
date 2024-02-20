e38ec1f7b81b2e604e307efeaaf6423d45cb7728
David Decker <david.decker@apptiva.ch>
2024-02-20 16:59:06 +0100

    update navbar items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import Link from 'next/link'
-import apptivaLogo from './logo.svg'
+import { ChevronDownIcon, ChevronUpIcon } from '@sanity/icons'
 import Image from 'next/image'
-import Button from './ui/button'
+import Link from 'next/link'
 import { useState } from 'react'
+import apptivaLogo from './logo.svg'
+import Button from './ui/button'
 
 const navbarData = [
   {
@@ -15,59 +16,26 @@ const navbarData = [
       {
         title: 'Development',
         text: '',
-        href: '/development/',
+        href: '/development',
       },
       {
-        title: 'Chatbot',
+        title: 'Chatbots',
         text: '',
-        href: '/chatbot/',
-      },
-      {
-        title: 'Consulting',
-        text: '',
-        href: '/consulting/',
+        href: '/chatbots',
       },
     ],
   },
-  { title: 'Projekte', href: '/projekte/' },
-  {
-    title: 'Apptiva',
-    href: '/ueber-uns/',
-    type: 'two',
-    items: [
-      {
-        title: 'Kultur & Firmenfacts',
-        text: 'Mehr über uns erfahren',
-        href: '/ueber-uns/',
-      },
-      {
-        title: 'Team',
-        text: 'Unser erfahrenes Entwicklungsteam',
-        href: '/ueber-uns/#team',
-      },
-      {
-        title: 'Kompetenzen',
-        text: 'Dies sind unsere Kompetenzen',
-        href: '/unsere-kompetenzen/',
-      },
-      {
-        title: 'Jobs',
-        text: 'Jetzt bewerben',
-        href: '/jobs/',
-      },
-    ],
-  },
-  { title: 'Blog', href: '/blog/' },
+  { title: 'Projekte', href: '/projekte' },
+  { title: 'Apptiva', href: '/ueber-uns' },
+  { title: 'Blog', href: '/blog' },
 ]
 
 const Navbar = () => {
-  const [activeNavigationItem, setActiveNavigationItem] = useState<
-    string | undefined
-  >(undefined)
+  const [showOffers, setShowOffers] = useState(false)
 
   return (
     <div className="fixed z-50 w-full p-4">
-      <div className="flex items-center justify-between rounded-md bg-[#053A78dd] px-8 py-6 text-base-white backdrop-blur-[6px]">
+      <div className="flex items-center justify-between rounded-md bg-primary-dark/40 px-8 py-6 text-base-white backdrop-blur-[6px]">
         <Link href={'/'}>
           <Image
             priority
@@ -76,33 +44,39 @@ const Navbar = () => {
             className="w-32"
           />
         </Link>
-        <nav className="flex justify-between gap-8 rounded bg-black/40 px-8 py-3">
-          {navbarData.map((item) => (
-            <div
-              key={item.href}
-              onMouseOver={() => setActiveNavigationItem(item.href)}
-              onMouseOut={() => setActiveNavigationItem(undefined)}
-            >
-              <ul key={item.href}>
-                <li key={item.href} className="relative">
-                  <Link key={item.href} href={item.href}>
-                    {item.title}
-                  </Link>
-                  {activeNavigationItem === item.href && item.items && (
-                    <div className="absolute left-[-1rem] rounded bg-primary-dark">
-                      <ul className="rounded bg-black/40 p-4">
+        <nav className="flex justify-between gap-8 rounded bg-primary px-8 py-3">
+          {navbarData.map((item) =>
+            item.type === 'one' ? (
+              <div key={item.href} onClick={() => setShowOffers(!showOffers)}>
+                <ul key={item.href}>
+                  <li key={item.href} className="relative flex flex-col">
+                    <div
+                      key={item.href}
+                      className={`flex cursor-pointer items-center gap-1 ${showOffers ? 'text-secondary' : ''}`}
+                    >
+                      {item.title}{' '}
+                      <span className="text-[1.5rem]">
+                        {showOffers ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                      </span>
+                    </div>
+                    {showOffers && (
+                      <ul className="full absolute left-[-2rem] right-[-16.75rem] top-10 rounded bg-primary p-4 ">
                         {item.items.map((subitem) => (
                           <li key={subitem.href}>
                             <Link href={subitem.href}>{subitem.title}</Link>
                           </li>
                         ))}
                       </ul>
-                    </div>
-                  )}
-                </li>
-              </ul>
-            </div>
-          ))}
+                    )}
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link key={item.href} href={item.href}>
+                {item.title}
+              </Link>
+            )
+          )}
         </nav>
         <Link href={'/kontakt/'}>
           <Button element="div" intent="secondary">
