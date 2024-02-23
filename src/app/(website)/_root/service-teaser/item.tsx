c76fbaf3d8d39c624a96b830b713213cb534120b
Carla Iten <carla.iten@apptiva.ch>
2024-02-23 11:03:40 +0100

    service-teasers: fixed corners, adjusted w and h of illustration

--- src/app/(website)/_root/service-teaser/item.tsx
+++ src/app/(website)/_root/service-teaser/item.tsx
@@ -14,12 +14,11 @@ const ServiceTeaser = ({
     <div className="full flex flex-col lg:flex-row">
       <div
         className={
-          'bg-base-grey max-lg:rounded-md lg:w-1/2 ' +
-          (rowNumber % 2 ? 'lg:rounded-r-md' : '  lg:rounded-l-md')
+          'bg-base-grey max-lg:rounded-t-md  lg:w-1/2 ' +
+          (rowNumber % 2 ? ' lg:rounded-r-md' : '  lg:rounded-l-md')
         }
       >
-        {/* <div className="m-28  w-8/12  text-primary"> */}
-        <div className="m-28  w-8/12  text-primary">
+        <div className="m-4  text-primary  lg:m-28 lg:w-8/12">
           <Heading level={2} size={3} className="pb-6">
             {service.header?.title}
           </Heading>
@@ -30,13 +29,13 @@ const ServiceTeaser = ({
       </div>
       <div
         className={
-          'flex items-center justify-center bg-primary max-lg:rounded-md lg:w-1/2' +
-          (rowNumber % 2 ? ' lg:rounded-l-md' : ' lg:rounded-r-md')
+          'flex items-center justify-center bg-primary max-lg:rounded-b-md lg:w-1/2' +
+          (rowNumber % 2 ? ' lg:rounded-l-md' : '  lg:rounded-r-md')
         }
       >
         <SanityImage
           image={service.illustration}
-          style="p-6 object-cover object-center"
+          style="p-6 object-cover object-center lg:max-w-96 lg:max-h-96"
         />
       </div>
     </div>
--- src/app/(website)/_root/service-teaser/list.tsx
+++ src/app/(website)/_root/service-teaser/list.tsx
@@ -1,10 +1,11 @@
-import ServiceTeaser from "./item"
-import { ServiceTeaserData } from "./types"
+import ServiceTeaser from './item'
+import { ServiceTeaserData } from './types'
 
-export default function ServiceTeaserList(props: {services: ServiceTeaserData[]}) {
-
+export default function ServiceTeaserList(props: {
+  services: ServiceTeaserData[]
+}) {
   return (
-    <div className="feature space-y-4 p-4 lg:[&>*:nth-child(even)]:flex-row-reverse ">
+    <div className="feature space-y-4 py-4 lg:p-4 lg:[&>*:nth-child(even)]:flex-row-reverse ">
       {props.services.map((service: any, index: number) => (
         <ServiceTeaser key={index} service={service} rowNumber={index} />
       ))}
