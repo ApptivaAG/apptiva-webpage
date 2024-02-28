b2598eb07150a6ff3a175a75496ef21fc88d1828
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-02-28 11:59:47 +0100

    Improved mobile font size

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -6,15 +6,15 @@ type Level = 1 | 2 | 3 | 4 | 5 | 6
 const getClassNameByLevel = (level: Level): string => {
   switch (level) {
     case 1:
-      return 'text-[3rem] leading-[3rem] lg:text-[5rem] font-[600] lg:leading-[5.5rem]'
+      return 'text-4xl lg:text-7xl'
     case 2:
-      return 'text-[2.5rem] leading-[2.7rem] lg:text-[4rem] font-[600] lg:leading-[4rem]'
+      return 'text-3xl lg:text-6xl'
     case 3:
-      return 'text-[2rem] leading-[2.2rem] lg:text-[3rem] font-[600] lg:leading-[3.125rem]'
+      return 'text-2xl lg:text-5xl'
     case 4:
-      return 'text-[1.6rem] leading-[1.8rem] lg:text-[2rem] font-[500] lg:leading-[2.5rem]'
+      return 'text-xl lg:text-3xl'
     case 5:
-      return 'text-[1.25rem] leading-[1.25rem] font-[600]'
+      return 'text-base'
     default:
       return ''
   }
@@ -33,6 +33,7 @@ export default function Heading(
     {
       className: cn(
         getClassNameByLevel(props.size ?? props.level),
+        'font-bold',
         props.className
       ),
     },
