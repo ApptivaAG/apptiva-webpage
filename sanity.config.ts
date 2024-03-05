4c4a251b71cbce32ec03a0c81925586560f203c6
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-03-05 10:01:14 +0100

    Added about to desk structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@
 
 import { visionTool } from '@sanity/vision'
 import { groqdPlaygroundTool } from 'groqd-playground'
-import { FaCog } from 'react-icons/fa'
+import { FaCog, FaAddressCard } from 'react-icons/fa'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { presentationTool } from 'sanity/presentation'
@@ -34,6 +34,15 @@ export default defineConfig({
                   .title('Einstellungen')
               )
               .icon(FaCog),
+            S.listItem()
+              .title('Über Apptiva Seite')
+              .child(
+                S.editor()
+                  .schemaType('about-page')
+                  .documentId('e068fd85-2bfd-41f5-9494-ba5c8691c0fa')
+                  .title('Über')
+              )
+              .icon(FaAddressCard),
 
             // List out the rest of the document types, but filter out the singleton types
             ...S.documentTypeListItems().filter(
