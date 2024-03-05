b0a46ff5def76ce58765e9fc1b58bc3844552a0b
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-03-05 18:50:58 +0100

    Cards in list are collapsible

--- src/components/module/cards/card-list.tsx
+++ src/components/module/cards/card-list.tsx
@@ -4,6 +4,12 @@ import StyledPortableText from '@/components/styled-portable-text'
 import { ModuleData } from '@/sanity/lib/queries'
 import { PortableText } from '@portabletext/react'
 import { Card } from '../../ui/card'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible'
+import { MinusCircle, PlusCircle } from 'lucide-react'
 
 export default function CardList(props: { module: ModuleData }) {
   const { module } = props
@@ -30,14 +36,25 @@ export default function CardList(props: { module: ModuleData }) {
           <div className="basis-7/12 space-y-10">
             {module.cards?.map((card) => (
               <Card key={card._key}>
-                <Heading level={isLevel(1) ? 3 : 4} size={4}>
-                  {card.title}
-                </Heading>
-                {card.content && (
-                  <div className="pt-6">
-                    <StyledPortableText content={card.content} />
+                <Collapsible>
+                  <div className="flex items-start justify-between gap-2">
+                    <Heading level={isLevel(1) ? 3 : 4} size={4}>
+                      {card.title}
+                    </Heading>
+                    <CollapsibleTrigger className="group grid *:size-8 *:transition-all *:[grid-area:1/1]">
+                      <PlusCircle className="rotate-0 scale-100 group-data-[state=open]:-rotate-90 group-data-[state=open]:scale-0" />
+                      <MinusCircle className="rotate-90 scale-0 group-data-[state=open]:rotate-0 group-data-[state=open]:scale-100" />
+                      <span className="sr-only">Aufklappen</span>
+                    </CollapsibleTrigger>
                   </div>
-                )}
+                  {card.content && (
+                    <CollapsibleContent className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
+                      <div className="pt-6">
+                        <StyledPortableText content={card.content} />
+                      </div>
+                    </CollapsibleContent>
+                  )}
+                </Collapsible>
               </Card>
             ))}
           </div>
