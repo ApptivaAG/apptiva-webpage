06a7dcda3d133318e97eee7dc52d0f1dde293118
David Decker <david.decker@apptiva.ch>
2024-03-12 16:24:29 +0100

    arrange service teasers with grid

--- src/components/module/service-teaser/service-teaser-list.tsx
+++ src/components/module/service-teaser/service-teaser-list.tsx
@@ -3,7 +3,7 @@ import ServiceTeaser from './service-teaser-item'
 
 export default function ServiceTeaserList(props: { module: ModuleData }) {
   return (
-    <div className="feature space-y-4 py-4 lg:p-4 lg:[&>*:nth-child(even)]:flex-row-reverse ">
+    <div className="feature grid gap-y-4 py-8">
       {props.module.servicePageTeaser?.map((service: any, index: number) => (
         <ServiceTeaser key={index} service={service} rowNumber={index} />
       ))}
