8ea4ba6cf4a9671ab093109ae2adc3cba99e1951
David Decker <david.decker@apptiva.ch>
2024-03-12 10:43:05 +0100

    use golden ratio in card flow

--- src/components/module/cards/card-flow.tsx
+++ src/components/module/cards/card-flow.tsx
@@ -24,61 +24,60 @@ export default function CardFlow(props: { module: ModuleData }) {
         )}
       >
         <div className="content">
-          <div className="space-y-20">
-            <div className="flex flex-wrap gap-x-32 gap-y-8">
-              <Heading level={isLevel(2) ? 3 : 2} size={isLevel(2) ? 4 : 3}>
-                {module.title}
-              </Heading>
-              <SanityImage image={module.image} />
-              {module.content && (
-                <div className="flex-1">
-                  <PortableText value={module.content} />
-                </div>
-              )}
+          <Heading
+            level={isLevel(2) ? 3 : 2}
+            size={isLevel(2) ? 4 : 3}
+            className="[grid-column:content-start/content-gap-start]"
+          >
+            {module.title}
+          </Heading>
+          {module.content && (
+            <div className="[grid-column:content-gap-end/content-end] max-md:mt-4">
+              <PortableText value={module.content} />
             </div>
-            <div className={cn('grid gap-7', colStyle)}>
-              {module.cards?.map((card) => {
-                const style = xor(darkBg, card.style !== 'inverted')
-                  ? 'dark'
-                  : 'light'
-                return (
-                  <Card
-                    key={card._key}
-                    className="relative flex flex-col gap-6"
-                    intent={style}
-                  >
-                    <div className="flex items-start gap-4">
-                      <SanityImage image={card.image} className="flex-none" />
-                      <Heading level={isLevel(2) ? 4 : 3} size={5}>
-                        {card.title}
-                      </Heading>
+          )}
+          <div className={cn('mt-20 grid gap-7', colStyle)}>
+            {module.cards?.map((card) => {
+              const style = xor(darkBg, card.style !== 'inverted')
+                ? 'dark'
+                : 'light'
+              return (
+                <Card
+                  key={card._key}
+                  className="relative flex flex-col gap-6"
+                  intent={style}
+                >
+                  <div className="flex items-start gap-4">
+                    <SanityImage image={card.image} className="flex-none" />
+                    <Heading level={isLevel(2) ? 4 : 3} size={5}>
+                      {card.title}
+                    </Heading>
+                  </div>
+                  {card.content && (
+                    <div
+                      className={cn(
+                        'flex flex-1 flex-col justify-between gap-6',
+                        card.pill && 'pb-8'
+                      )}
+                    >
+                      <StyledPortableText content={card.content} />
                     </div>
-                    {card.content && (
-                      <div
-                        className={cn(
-                          'flex flex-1 flex-col justify-between gap-6',
-                          card.pill && 'pb-8'
-                        )}
-                      >
-                        <StyledPortableText content={card.content} />
-                      </div>
-                    )}
-                    {card.pill && (
-                      <div
-                        className={cn(
-                          'rounded-full absolute -bottom-5 left-9 mr-9 line-clamp-2 px-7 py-3',
-                          style === 'light' &&
-                            'border bg-primary text-base-white',
-                          style === 'dark' && 'bg-base-white text-primary'
-                        )}
-                      >
-                        {card.pill}
-                      </div>
-                    )}
-                  </Card>
-                )
-              })}
-            </div>
+                  )}
+                  {card.pill && (
+                    <div
+                      className={cn(
+                        'absolute -bottom-5 left-9 mr-9 line-clamp-2 rounded-full px-7 py-3',
+                        style === 'light' &&
+                          'border bg-primary text-base-white',
+                        style === 'dark' && 'bg-base-white text-primary'
+                      )}
+                    >
+                      {card.pill}
+                    </div>
+                  )}
+                </Card>
+              )
+            })}
           </div>
         </div>
       </section>
