18db8f3306847e6a83017628941fed01cfc01aa2
Carla Iten <carla.iten@apptiva.ch>
2024-03-13 11:41:34 +0100

    use new feature grid columns for fullscreen image

--- src/components/module/fullscreen-image.tsx
+++ src/components/module/fullscreen-image.tsx
@@ -6,18 +6,13 @@ import StyledPortableText from '../styled-portable-text'
 export default function FullscreenImage(props: { module: ModuleData }) {
   return (
     <div className="full relative flex items-end">
-      {/* fill: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, 
-      rgba(0, 0, 0, 0.40) 100%), url(<path-to-image>), lightgray 50% / cover no-repeat;
-       */}
-
       <SanityImage
         className=" object-cover"
         image={props.module.fullscreenImage}
       />
       {props.module.fullscreenImageText && (
-        <div className="content absolute bg-transparent text-base-white lg:pb-36">
-          <div className="lg:w-2/3">
-            {' '}
+        <div className="content absolute bg-transparent text-base-white *:[grid-column:feature-start/content-end] lg:pb-36  ">
+          <div className="md:pb-[5%] xl:w-2/3 xl:pb-[10%]">
             <StyledPortableText
               content={props.module.fullscreenImageText}
               spreadParagraphs
