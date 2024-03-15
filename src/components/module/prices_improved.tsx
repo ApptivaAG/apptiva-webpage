61efd4359e67b47ba0faeeac79590de367c47ebd
Carla Iten <carla.iten@apptiva.ch>
2024-03-15 12:36:15 +0100

    added price pill and favourite pill to prices cards

--- src/components/module/prices_improved.tsx
+++ src/components/module/prices_improved.tsx
@@ -8,6 +8,7 @@ import { PortableText } from '@portabletext/react'
 import { HandHeart } from 'lucide-react'
 import Button from '../ui/button'
 import StyledPortableText from '../styled-portable-text'
+import { cn } from '@/utils/cn'
 
 export default function PricesImproved(props: { module: ModuleData }) {
   const { module } = props
@@ -30,30 +31,58 @@ export default function PricesImproved(props: { module: ModuleData }) {
               </div>
             )}
           </div>
-          <div className=" grid gap-7 gap-y-14 lg:grid-cols-3 ">
+          <div className=" grid gap-7 gap-y-14 lg:grid-cols-3">
             {props.module.prices?.map((price, index) => (
-              <div key={price._key} className="grid grid-rows-3 ">
+              <div
+                key={price._key}
+                className="grid grid-rows-3 rounded-lg bg-base-white"
+              >
+                {/* upper part: blue or green */}
                 <div
                   className={
-                    '  row-span-1 row-start-1  flex w-full flex-col items-center rounded-lg border-secondary p-9 pb-10  ' +
+                    'relative  row-span-1 row-start-1 flex w-full flex-col items-center justify-center rounded-lg border-secondary p-9   ' +
                     (index % 2
                       ? 'border-secondary bg-secondary text-primary'
                       : 'bg-primary text-base-white')
                   }
                 >
-                  <Heading level={4} size={4} className="gap-6">
+                  {/* show favourite badge */}
+                  {price.isFavourite && (
+                    // cn(
+                    //   'absolute -bottom-5 left-9 mr-9 line-clamp-2 rounded-full px-7 py-3',
+                    //   style === 'light' &&
+                    //     'border bg-primary text-base-white',
+                    //   style === 'dark' && 'bg-base-white text-primary'
+                    // )
+                    <div
+                      className={cn(
+                        'absolute -top-5 line-clamp-2 flex justify-items-center rounded-full  bg-primary  px-7 py-3 text-base-white',
+                        !(index % 2) && '-top-6 border bg-primary'
+                      )}
+                    >
+                      <HandHeart className=" h-5 w-5 shrink-0 transition-transform duration-200" />
+                      <div className=" pl-2">Favorit</div>
+                    </div>
+                  )}
+                  <Heading level={4} size={4} className="gap-6 pb-2">
                     {price.title}
                   </Heading>
                   <p className="text-center">{price.subtitle}</p>
+                  {/* pill part */}
+                  <div className="absolute -bottom-6 line-clamp-2 rounded-full bg-base-grey px-7 py-3 text-primary">
+                    {price.pillText}
+                  </div>
                 </div>
-                <div className="row-span-2 ">
+
+                {/* lower part: always on white bg */}
+                <div className="row-span-2 bg-base-white pl-[20%] pt-16 lg:pl-4">
                   {price.content && (
                     <div className="self-stretch">
                       <StyledPortableText content={price.content} />
                     </div>
                   )}
                 </div>
-                <div className=" flex items-center justify-center rounded-b-lg bg-base-white  p-12">
+                <div className=" flex items-center justify-center p-10 pb-12">
                   <Button
                     element="div"
                     intent={index % 2 ? 'secondary' : 'primary'}
