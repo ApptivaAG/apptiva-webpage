b1fe6644d755c8e637ec0c3973d102de48ec4ec2
Carla Iten <carla.iten@apptiva.ch>
2024-03-18 18:37:31 +0100

    removed unused compnentes from home as they are already in modules

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -7,11 +7,6 @@ export default async function Home() {
     <>
       <Hero />
       <ModulesPage />
-      {/* <ServiceTeasers />
-      
-      <Testimonials />      
-      <Customers />
-      */}
     </>
   )
 }
