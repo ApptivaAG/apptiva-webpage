8691c73b02a939b9ebd0202e3a2587ea1aec37d8
Kevin Rickenbach <kevin.rickenbach@apptiva.ch>
2024-03-19 15:12:33 +0100

    add introduction text to "wissen"

--- src/app/(website)/wissen/page.tsx
+++ src/app/(website)/wissen/page.tsx
@@ -27,8 +27,30 @@ export default async function Knowledge() {
         links={[{ name: 'Wissen', href: '/wissen' }]}
       />
       <section className="full py-16 text-primary">
+        <div className="content">
+          <Heading level={2} size={3}>
+            WISSEN wird bei uns gross geschrieben
+          </Heading>
+          <div className="mt-4 max-w-2xl lg:mt-6">
+            <p>
+              In unseren wöchentlichen Wissensaustausch-Sitzungen vermitteln wir
+              untereinander abwechselnd neues Wissen. Unser Ziel ist es, dieses
+              Wissen zu teilen und nicht für uns zu behalten. Deshalb haben wir
+              es für dich aufgeschrieben: Durchstöbere unsere Blogposts,
+              erweitere deinen Wortschatz mit unserem Glossar oder finde
+              Antworten auf deine Fragen in unserem FAQ. Drückt der Schuh bei
+              einem Thema, das wir noch nicht behandelt haben, kontaktiere uns
+              gerne – wir sind möglicherweise in der Lage, dir weiterzuhelfen.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="full py-16 text-primary">
         <div className="content space-y-4">
-          <Heading level={2}>Neueste Blogposts</Heading>
+          <Heading level={2} size={3}>
+            Neueste Blogposts
+          </Heading>
           <div>
             <Link href={'/blog/'}>
               <Button element="div" className="inline">
