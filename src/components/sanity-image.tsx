7ac2e2e59fe7b2c36a2f3bc8718240f778808b78
David Decker <david.decker@apptiva.ch>
2024-03-19 15:41:34 +0100

    use image dimensions for sanity image size check

--- src/components/sanity-image.tsx
+++ src/components/sanity-image.tsx
@@ -10,10 +10,9 @@ const SanityImage = ({
   image: (SanityImageWithAlt & { asset?: any }) | null
   className?: string
 }) => {
-  const imageIdInformation: string[] = image?.asset._id.split('-')
-  const [width, height] = imageIdInformation
-    ? imageIdInformation[imageIdInformation.length - 2].split('x')
-    : []
+  const { width = 0, height = 0 } = image?.asset
+    ? getImageDimensions(image)
+    : {}
 
   return image?.asset ? (
     <Image
@@ -21,9 +20,9 @@ const SanityImage = ({
       key={image.toString()}
       src={urlForImage(image).url()}
       alt={image.alt}
-      width={getImageDimensions(image).width}
-      height={getImageDimensions(image).height}
-      {...((+width > 40 || +height > 40) && { placeholder: 'blur' })}
+      width={width}
+      height={height}
+      {...((width > 40 || height > 40) && { placeholder: 'blur' })}
       blurDataURL={getImageAsset(image).metadata.lqip}
       sizes="(max-width: 768px) 100vw,
         (max-width: 1200px) 50vw,
