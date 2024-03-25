9067f2a842bbe55142e69b8100dd864477902035
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-03-25 11:15:02 +0100

    Simple animated underline

--- src/app/(website)/_root/hero/content.tsx
+++ src/app/(website)/_root/hero/content.tsx
@@ -1,8 +1,5 @@
 import { SettingsDataQueries } from '@/sanity/lib/queries'
 import StyledPortableText from '../../../../components/styled-portable-text'
-import { PortableTextComponents, toHTML } from '@portabletext/to-html'
-import { groupBy } from 'remeda'
-import Typewriter from '@/components/typewriter'
 
 export default async function Content(props: {
   claim: NonNullable<SettingsDataQueries>
@@ -11,27 +8,9 @@ export default async function Content(props: {
     return <p>No Claim</p>
   }
 
-  const myPortableTextComponents: PortableTextComponents = {
-    marks: {
-      underline: ({ children }) => `<u>${children}</u>`,
-    },
-    block: {
-      h1: ({ children }) => children ?? '',
-    },
-  }
-
-  const blocks = groupBy(props.claim.claim, (x) => x.style)
-
   return (
-    <div className="content space-y-8">
-      {blocks.h1 && (
-        <Typewriter
-          text={toHTML(blocks.h1, { components: myPortableTextComponents })}
-        />
-      )}
-      {blocks.normal && (
-        <StyledPortableText content={blocks.normal} spreadParagraphs />
-      )}
+    <div className="content">
+      <StyledPortableText content={props.claim.claim} spreadParagraphs />
     </div>
   )
 }
