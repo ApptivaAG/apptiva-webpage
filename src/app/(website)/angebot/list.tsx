36ab1167d274d0ff40da79c4d33be2deb7774df9
David Decker <david.decker@apptiva.ch>
2024-03-26 18:47:59 +0100

    handle blocks title in service page list

--- src/app/(website)/angebot/list.tsx
+++ src/app/(website)/angebot/list.tsx
@@ -1,17 +1,24 @@
-import Link from "next/link";
-import { ServiceQueryData } from "./types";
+import Link from 'next/link'
+import { ServiceQueryData } from './types'
 
-export default function ServiceList(props: {services: ServiceQueryData[]}) {
-
+export default function ServiceList(props: { services: ServiceQueryData[] }) {
   return (
     <ul>
-    {props.services.map((servicePage) => (
-      <li key={servicePage.slug}>
-        <Link href={'angebot/' + servicePage.slug}>
-          {servicePage.header?.title}
-        </Link>
-      </li>
-    ))}
-  </ul>
-)
+      {props.services.map((servicePage) =>
+        servicePage.header?.title ? (
+          <li key={servicePage.slug}>
+            <Link href={'angebot/' + servicePage.slug}>
+              {servicePage.header?.title
+                .flatMap((block: { children: { text: string }[] }) =>
+                  block.children.map((child) => child.text)
+                )
+                .join('')}
+            </Link>
+          </li>
+        ) : (
+          <></>
+        )
+      )}
+    </ul>
+  )
 }
