e0019d171604e784e1b3682a1cbdb740f6747be8
David Decker <david.decker@apptiva.ch>
2024-03-26 16:22:30 +0100

    change header title to blocks

--- src/sanity/schemas/objects/header.tsx
+++ src/sanity/schemas/objects/header.tsx
@@ -8,13 +8,30 @@ export default defineType({
     defineField({
       name: 'title',
       title: 'Titel',
-      type: 'string',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {
+              title: 'Normal',
+              value: 'normal',
+              component: (props) => <h1>{props.children}</h1>,
+            },
+          ],
+          lists: [],
+          marks: {
+            decorators: [{ title: 'Underline', value: 'underline' }],
+            annotations: [],
+          },
+        },
+      ],
     }),
     defineField({
       name: 'lead',
       title: 'Einleitung',
       type: 'text',
-      rows: 3
+      rows: 3,
     }),
     defineField({
       name: 'image',
@@ -28,6 +45,6 @@ export default defineType({
       name: 'meta',
       title: 'Metadata',
       type: 'meta',
-    })
+    }),
   ],
 })
