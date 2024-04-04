63eb96f567bace063452da4c53b8151b0138d674
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-04-04 18:30:22 +0200

    Added shimmer effect if lqip blur is missing

--- src/components/sanity-image.tsx
+++ src/components/sanity-image.tsx
@@ -2,9 +2,10 @@ import { dataset, projectId } from '@/sanity/env'
 import { urlForImage } from '@/sanity/lib/image'
 import { SanityImageWithAlt } from '@/utils/types'
 import { getImageAsset, getImageDimensions } from '@sanity/asset-utils'
+import type { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 
-const SanityImage = ({
+export default function SanityImage({
   image,
   className,
   size,
@@ -14,7 +15,7 @@ const SanityImage = ({
   className?: string
   size?: 'content' | 'popout' | 'full' | 'default'
   sizes?: string
-}) => {
+}) {
   if (!image) return null
   const { width = 0, height = 0 } = image?.asset
     ? getImageDimensions(image)
@@ -25,7 +26,7 @@ const SanityImage = ({
     dataset: dataset,
   })
 
-  return image?.asset ? (
+  return asset ? (
     <Image
       className={className}
       key={image.toString()}
@@ -33,9 +34,7 @@ const SanityImage = ({
       alt={image.alt}
       width={width}
       height={height}
-      {...(asset.metadata.lqip &&
-        (width > 40 || height > 40) && { placeholder: 'blur' })}
-      blurDataURL={asset.metadata.lqip}
+      placeholder={buildPlaceholder(asset.metadata.lqip, width, height)}
       sizes={
         sizes
           ? sizes
@@ -51,4 +50,38 @@ const SanityImage = ({
   ) : null
 }
 
-export default SanityImage
+function buildPlaceholder(
+  lqip: string | undefined,
+  width: number,
+  height: number
+): PlaceholderValue | undefined {
+  const isTooSmallForBlur = width < 40 || height < 40
+
+  if (isTooSmallForBlur) {
+    return undefined
+  }
+
+  return (
+    (lqip as `data:image/${string}`) ??
+    `data:image/svg+xml;base64,${toBase64(shimmer(width, height))}`
+  )
+}
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#eee" offset="20%" />
+      <stop stop-color="#ddd" offset="50%" />
+      <stop stop-color="#eee" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#eee" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
