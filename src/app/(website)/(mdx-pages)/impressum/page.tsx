757c748ce7ee7ec0271d92fb9d920133ebfd300a
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-04-11 13:55:17 +0200

    Fixed mdx content

--- src/app/(website)/(mdx-pages)/impressum/page.tsx
+++ src/app/(website)/(mdx-pages)/impressum/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from 'next'
 import Wrapper from '../wrapper'
-import Content, { meta } from './content.mdx'
+import Content from './content.mdx'
 
-export const metadata = meta
+export const metadata: Metadata = {
+  title: 'Impressum',
+  description:
+    'Impressum der Webseite der Apptiva AG. Kontakt: Apptiva AG, Eichweid 1, 6203 Sempach Station, Schweiz, 041 322 26 26, info@apptiva.ch',
+  alternates: { canonical: '/impressum' },
+  openGraph: {
+    title: 'Impressum',
+    url: '/impressum',
+  },
+}
 
 export default function Impressum() {
   return (
