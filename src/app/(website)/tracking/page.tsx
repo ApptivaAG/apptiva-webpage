b249c80665f67010fc4e211887331197d038f407
David Decker <david.decker@apptiva.ch>
2024-04-16 10:59:06 +0200

    add tracking page

---
+++ src/app/(website)/tracking/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Heading from '@/components/heading'
+import Section from '@/components/section'
+import Button from '@/components/ui/button'
+import { useLocalStorage } from '@/utils/use-local-storage'
+
+export default function Tracking() {
+  const [ignore, setIgnore] = useLocalStorage('plausible_ignore', false)
+
+  return (
+    <Section className="content">
+      <Heading level={1} size={3}>
+        Analytics ausschliessen
+      </Heading>
+      <p className="pb-4 pt-12">
+        Klicke auf den unten stehenden Button, um deine Analytics-Einstellungen
+        zu ändern.
+      </p>
+      <p>
+        Du schliesst deine Besuche aktuell <b>{ignore ? 'aus' : 'nicht aus'}</b>
+        .
+      </p>
+      <Button
+        element="div"
+        className="my-12 w-fit"
+        onClick={() => setIgnore(!ignore)}
+      >
+        Meine Besuche {ignore ? 'nicht mehr' : ''} ausschliessen
+      </Button>
+    </Section>
+  )
+}
