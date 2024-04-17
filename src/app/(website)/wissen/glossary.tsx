2c924b7d7a1764e0b24a8a9c8834554780a37a30
Carla Iten <carla.iten@apptiva.ch>
2024-04-17 16:04:57 +0200

    changed glossar introduction text on wissen page

--- src/app/(website)/wissen/glossary.tsx
+++ src/app/(website)/wissen/glossary.tsx
@@ -16,22 +16,17 @@ export default function Glossar(props: { data: GlossaryQueryData }) {
           </Heading>
           <div className="col-right max-lg:mt-4">
             <p>
-              Hier haben wir alle wichtigen Fachbegriffe rund um die Themen
-              Chatbot und Conversational AI für dich zusammengestellt.
+              Tauche ein in unser Glossar und erweitere dein Verständnis über
+              Begriffe wie Chatbots, KI, und mehr.
+              <br />
+              <Link className="self-end" href={`/glossar`}>
+                <UnderlineForLink>Alle Begriffe →</UnderlineForLink>
+              </Link>
             </p>
           </div>
           {glossaryEntries && (
             <GlossaryComponent glossaryEntries={glossaryEntries.slice(0, 5)} />
           )}
-          <div className="col-right mt-8">
-            <p>
-              Tauche ein in unser Glossar und erweitere dein Verständnis über
-              Begriffe wie Chatbots, KI, und mehr.
-              <Link className="self-end pl-2" href={`/glossar`}>
-                <UnderlineForLink>→&ensp;Alle Begriffe</UnderlineForLink>
-              </Link>
-            </p>
-          </div>
         </div>
       </section>
     </>
