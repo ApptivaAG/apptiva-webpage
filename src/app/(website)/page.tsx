071f17a3ee317ea7a85e5ebb6995bf520c990081
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-05-07 09:46:25 +0200

    Changed title on home page to special title

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,6 +1,18 @@
 import Hero from '@/app/(website)/_root/hero'
 
 import ModulesPage from './modules/page'
+import { Metadata } from 'next'
+
+const title =
+  'Apptiva AG | Chatbots und individuelle Softwarelösungen aus der Schweiz'
+export const metadata: Metadata = {
+  title: {
+    absolute: title,
+  },
+  openGraph: {
+    title,
+  },
+}
 
 export default async function Home() {
   return (
