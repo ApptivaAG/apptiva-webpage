8b42ce6e84b9605407f1b009723d3f73ae22750b
David Decker <david.decker@apptiva.ch>
2024-05-08 14:20:41 +0200

    reanimate underline when hovered

--- src/components/ui/underline.tsx
+++ src/components/ui/underline.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { cn } from '@/utils/cn'
+import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import style from './underline.module.css'
 
@@ -8,19 +9,31 @@ export default function Underline(props: {
   children?: React.ReactNode
   className?: string
 }) {
+  const [hovered, setHovered] = useState(false)
+  const [key, setKey] = useState(0)
+
   const { ref, inView } = useInView({
     triggerOnce: true,
     rootMargin: '0px 0px -20% 0px',
     threshold: 1,
   })
+
+  useEffect(() => {
+    if (hovered) setKey((k) => k + 1)
+  }, [hovered])
+
   return (
     <span
       ref={ref}
+      key={key}
       className={cn(
         style['highlighted-text'],
-        inView && style.animate,
+        inView && !hovered && style.animate,
+        hovered && style.animate,
         props.className
       )}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
       {props.children}
     </span>
