bc78ce962df2a6e056851f8621cb06dc2b552ea4
David Decker <david.decker@apptiva.ch>
2024-06-12 09:52:55 +0200

    use accordion for card list

--- src/components/module/cards/card-list.tsx
+++ src/components/module/cards/card-list.tsx
@@ -1,12 +1,12 @@
-import Section from '@/components/section'
 import Heading from '@/components/heading'
 import SanityImage from '@/components/sanity-image'
+import Section from '@/components/section'
 import StyledPortableText from '@/components/styled-portable-text'
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/components/ui/collapsible'
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+} from '@/components/ui/accordion'
 import { ModuleData } from '@/sanity/lib/queries'
 import { PortableText } from '@portabletext/react'
 import { PiMinusCircle, PiPlusCircle } from 'react-icons/pi'
@@ -37,32 +37,44 @@ export default function CardList(props: { module: ModuleData }) {
         <div className="col-right space-y-4 max-lg:mt-10">
           {module.cards?.map((card) => (
             <Card key={card._key} padding="small">
-              <Collapsible>
-                <CollapsibleTrigger className="w-full">
-                  <div className="grid auto-cols-auto grid-flow-col items-start justify-between gap-2">
-                    <Heading
-                      level={isLevel(1) ? 3 : 4}
-                      size={4}
-                      className=" text-start md:text-2xl"
+              <Accordion
+                className="w-full divide-y"
+                transition
+                transitionTimeout={200}
+              >
+                <AccordionItem
+                  className="w-full py-0"
+                  header={
+                    <div className="grid auto-cols-auto grid-flow-col items-start justify-between gap-2">
+                      <Heading
+                        level={isLevel(1) ? 3 : 4}
+                        size={4}
+                        className="text-start md:text-2xl"
+                      >
+                        {card.title}
+                      </Heading>
+                    </div>
+                  }
+                  icon={
+                    <div
+                      className="grid justify-self-end *:size-8 *:transition-all *:[grid-area:1/1]"
+                      data-state="closed"
                     >
-                      {card.title}
-                    </Heading>
-
-                    <div className="group grid justify-self-end  *:size-8 *:transition-all *:[grid-area:1/1]">
-                      <PiPlusCircle className="rotate-0 scale-100 group-data-[state=open]:-rotate-90 group-data-[state=open]:scale-0" />
-                      <PiMinusCircle className="rotate-90 scale-0 group-data-[state=open]:rotate-0 group-data-[state=open]:scale-100" />
+                      <PiPlusCircle className="rotate-0 scale-100 group-aria-expanded:-rotate-90 group-aria-expanded:scale-0" />
+                      <PiMinusCircle className="rotate-90 scale-0 group-aria-expanded:rotate-0 group-aria-expanded:scale-100" />
                       <span className="sr-only">Aufklappen</span>
                     </div>
-                  </div>
-                </CollapsibleTrigger>
-                {card.content && (
-                  <CollapsibleContent className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
-                    <div className="pt-6">
-                      <StyledPortableText content={card.content} />
-                    </div>
-                  </CollapsibleContent>
-                )}
-              </Collapsible>
+                  }
+                >
+                  {card.content && (
+                    <AccordionContent className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
+                      <div className="pt-6">
+                        <StyledPortableText content={card.content} />
+                      </div>
+                    </AccordionContent>
+                  )}
+                </AccordionItem>
+              </Accordion>
             </Card>
           ))}
         </div>
--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,13 +1,13 @@
 'use client'
 
+import { cn } from '@/utils/cn'
 import {
   Accordion as AccordionPrimitive,
   AccordionItem as Item,
 } from '@szhsin/react-accordion'
 import * as React from 'react'
 import { IoChevronDown } from 'react-icons/io5'
-
-import { cn } from '@/utils/cn'
+import { IconType } from 'react-icons/lib'
 
 const Accordion = AccordionPrimitive
 
@@ -15,23 +15,27 @@ const AccordionItem = ({
   header,
   children,
   className,
+  icon,
   ...rest
 }: {
   header: React.ReactNode
   children: React.ReactNode
   className?: string
+  icon?: React.ReactNode
 }) => (
   <Item
     {...rest}
     header={({ state: { isEnter } }) => (
       <>
         {header}
-        <IoChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200 " />
+        {icon ?? (
+          <IoChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200 " />
+        )}
       </>
     )}
     buttonProps={{
       className: cn(
-        'flex justify-between hover:underline items-center w-full py-4 text-left hover:bg-slate-100 [&[aria-expanded=true]>svg]:rotate-180',
+        'flex justify-between hover:underline items-center w-full py-4 text-left hover:bg-slate-100 [&[aria-expanded=true]>svg]:rotate-180 group',
         className
       ),
     }}
