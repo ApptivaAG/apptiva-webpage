a1dc03fc315292f667948d326d2c2ee1dbabc968
Carla Iten <carla.iten@apptiva.ch>
2024-08-29 11:58:50 +0200

    refactored filter on project page

--- src/app/(website)/projekte/list.tsx
+++ src/app/(website)/projekte/list.tsx
@@ -36,32 +36,15 @@ export default function ProjectList(props: { projects: ProjectQueryData[] }) {
         lead="Eine Auswahl unserer Projekte der letzten 10 Jahre."
         links={[{ name: 'Projekte', href: '/projekte' }]}
       />
-
       <div className="flex gap-4 pt-16">
-        <Button
-          intent={getIntent(topics[0].value)}
-          onClick={() => setTopic(topics[0].value)}
-        >
-          {topics[0].name}
-        </Button>
-        <Button
-          intent={getIntent(topics[1].value)}
-          onClick={() => setTopic(topics[1].value)}
-        >
-          {topics[1].name}
-        </Button>
-        <Button
-          intent={getIntent(topics[2].value)}
-          onClick={() => setTopic(topics[2].value)}
-        >
-          {topics[2].name}
-        </Button>
-        <Button
-          intent={getIntent(topics[3].value)}
-          onClick={() => setTopic(topics[3].value)}
-        >
-          {topics[3].name}
-        </Button>
+        {topics.map((topic) => (
+          <Button
+            intent={getIntent(topic.value)}
+            onClick={() => setTopic(topic.value)}
+          >
+            {topic.name}
+          </Button>
+        ))}
       </div>
 
       <ul className="grid gap-4 py-16 lg:grid-cols-3">
