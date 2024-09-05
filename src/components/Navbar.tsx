abb72c8435a04b1454f0170b1ec2c3253829a3fe
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-09-05 17:39:55 +0200

    Fixed safari bug in footer scrolling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => (
       <MainNav />
       <MobileNav />
 
-      <NextLink href={'/kontakt/'} className="hidden md:block">
+      <NextLink href={'/kontakt'} className="hidden md:block">
         <Button element="div" intent="secondary">
           Kontakt
         </Button>
