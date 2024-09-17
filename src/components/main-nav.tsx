6d559009371f1e0755f812957ae4f5f3407843d1
Carla Iten <carla.iten@apptiva.ch>
2024-09-17 16:00:05 +0200

    conditional padding if cta button

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -60,6 +60,7 @@ const MainNav = () => {
                           </Link>
                         ))}
                     </div>
+                    {/* <div className="col-start-2 p-2 py-6"> */}
                     <div className="col-start-2 self-center p-2">
                       {item.items
                         .filter((item) => item.type === 'link')
@@ -77,28 +78,29 @@ const MainNav = () => {
                           </Link>
                         ))}
                     </div>
+                    {item.items.find((item) => item.type === 'cta-link') && (
+                      <div className="col-start-2 max-w-fit px-2 pb-6">
+                        {item.items
+                          .filter((item) => item.type === 'cta-link')
+                          .map((subitem, index) => (
+                            <Link
+                              href={subitem.href}
+                              key={
+                                subitem.href + ' ' + subitem.title + ' ' + index
+                              }
+                              className="rounded-md hover:bg-primary-light/10"
+                            >
+                              <div className="flex flex-col">
+                                {/* <span className="pb-1 pt-1">{subitem.title}</span> */}
 
-                    <div className="col-start-2 max-w-fit px-2 pb-6">
-                      {item.items
-                        .filter((item) => item.type === 'cta-link')
-                        .map((subitem, index) => (
-                          <Link
-                            href={subitem.href}
-                            key={
-                              subitem.href + ' ' + subitem.title + ' ' + index
-                            }
-                            className="rounded-md hover:bg-primary-light/10"
-                          >
-                            <div className="flex flex-col">
-                              {/* <span className="pb-1 pt-1">{subitem.title}</span> */}
-
-                              <Button intent="secondary">
-                                {subitem.title}
-                              </Button>
-                            </div>
-                          </Link>
-                        ))}
-                    </div>
+                                <Button intent="secondary">
+                                  {subitem.title}
+                                </Button>
+                              </div>
+                            </Link>
+                          ))}
+                      </div>
+                    )}
                   </NavigationMenuContent>
                 )}
               </>
