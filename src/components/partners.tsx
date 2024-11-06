bcbc6dd05973e83c85d1c102204145ab180f12e2
Carla Iten <carla.iten@apptiva.ch>
2024-11-06 14:30:40 +0100

    aligned partner module title with other titles

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -20,35 +20,37 @@ export default function Partners(props: {}) {
     )
 
   return (
-    <div className="content">
-      <Heading level={2} size={3} className="text-primary">
-        Unsere Partner
-      </Heading>
-      <Carousel>
-        <div className="flex h-[16.4rem] items-center">
-          {partners.map((partner: any) => {
-            return (
-              <div
-                key={partner.id}
-                className="relative mr-4 min-w-0 flex-[0_0_16.4rem] bg-base-white md:mr-16"
-                style={{ height: partner.height + 10 }}
-              >
-                <WithLink link={partner.link}>
-                  <Image
-                    alt={partner.alt}
-                    src={partner.path}
-                    fill
-                    className="object-contain opacity-80"
-                  />
+    <div className="full py-8 lg:py-28">
+      <div className="content">
+        <Heading level={2} size={3} className="text-primary">
+          Unsere Partner
+        </Heading>
 
-                  <div className="absolute inset-[1px] bg-primary bg-blend-lighten mix-blend-color [filter:brightness(120%)]"></div>
-                </WithLink>
-              </div>
-              // </WithLink>
-            )
-          })}
-        </div>
-      </Carousel>
+        <Carousel>
+          <div className="content justify-content: space-evenly flex h-[16.4rem] items-center justify-evenly">
+            {partners.map((partner: any) => {
+              return (
+                <div
+                  key={partner.id}
+                  className="relative mr-4 min-w-0 flex-[0_0_16.4rem] bg-base-white md:mr-16"
+                  style={{ height: partner.height + 10 }}
+                >
+                  <WithLink link={partner.link}>
+                    <Image
+                      alt={partner.alt}
+                      src={partner.path}
+                      fill
+                      className="object-contain opacity-80"
+                    />
+
+                    <div className="absolute inset-[1px] bg-primary bg-blend-lighten mix-blend-color [filter:brightness(120%)]"></div>
+                  </WithLink>
+                </div>
+              )
+            })}
+          </div>
+        </Carousel>
+      </div>
     </div>
   )
 }
