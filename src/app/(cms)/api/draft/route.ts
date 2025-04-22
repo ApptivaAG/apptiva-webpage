9e93dd8d8dfa0945bf191084651d9b1a6709b5d6
Carla Iten <carla.iten@apptiva.ch>
2025-04-22 15:09:49 +0200

    trying to prevent timeout error when loading studio page

--- src/app/(cms)/api/draft/route.ts
+++ src/app/(cms)/api/draft/route.ts
@@ -1,4 +1,5 @@
 // ./app/api/draft/route.ts
+export const runtime = 'edge' // 👈 Enable Edge runtime!
 
 import { validatePreviewUrl } from '@sanity/preview-url-secret'
 import { draftMode } from 'next/headers'
@@ -10,16 +11,17 @@ import { token } from '@/sanity/env'
 const clientWithToken = client.withConfig({ token })
 
 export async function GET(request: Request) {
+  console.time('[api/draft] validatePreviewUrl (edge)')
   const { isValid, redirectTo = '/' } = await validatePreviewUrl(
     clientWithToken,
     request.url
   )
+  console.timeEnd('[api/draft] validatePreviewUrl (edge)')
 
   if (!isValid) {
     return new Response('Invalid secret', { status: 401 })
   }
 
   draftMode().enable()
-
   redirect(redirectTo)
 }
