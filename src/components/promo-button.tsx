a684a8603f495891aa812ed9ae9f674add3e6eb2
David Decker <david.decker@apptiva.ch>
2025-12-01 17:08:52 +0100

    bounce promo icon with tailwind

--- src/components/promo-button.tsx
+++ src/components/promo-button.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import icon from './promo-button.svg'
+import icon from './promo-button-icon.svg'
 
 const PromoButton = () => {
   const [show, setShow] = useState(false)
@@ -26,10 +26,7 @@ const PromoButton = () => {
   return (
     <Link
       href="/angebot/chatbots/weihnachtsaktion"
-      className="absolute bottom-4 right-4 z-50 md:bottom-16 md:right-16"
-      style={{
-        animation: 'bounce 1s ease-in-out 3.5',
-      }}
+      className="absolute bottom-4 right-4 z-50 animate-[bounce_1s_ease-in-out_3.5] md:bottom-16 md:right-16"
     >
       <Image
         priority
