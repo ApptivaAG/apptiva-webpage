03467a695a1a49d456c4177811d7716aece7b112
Carla Iten <carla.iten@apptiva.ch>
2026-01-26 17:36:35 +0100

    move text to bottom if there's no preview

--- src/components/module/docs/doc.tsx
+++ src/components/module/docs/doc.tsx
@@ -1,15 +1,7 @@
 import Heading from '@/components/heading'
-import Section from '@/components/section'
-import { ModuleData } from '@/sanity/lib/queries'
-import { cleanStega } from '@/utils/clean-stega'
-import { formatIds } from '@/utils/format-ids'
-import { PortableText } from '@portabletext/react'
-import { moduleStyleToSectionIntent } from '../utils'
-import { cn } from '@/utils/cn'
-import { Card } from '../../ui/card'
-import SanityImage from '../../sanity-image'
-import { FaArrowRight, FaFilePdf, FaLink } from 'react-icons/fa'
 import Link from 'next/link'
+import SanityImage from '../../sanity-image'
+import { Card } from '../../ui/card'
 import UnderlineForLink from '../../ui/underline-for-link'
 
 export default function Doc(props: { doc: any }) {
@@ -18,10 +10,7 @@ export default function Doc(props: { doc: any }) {
   const linkHref = fileUrl || doc.externalLink
 
   return (
-    <Card
-      className="border-slate-100 bg-white flex h-full flex-col gap-5 p-8"
-      intent="light"
-    >
+    <Card className="flex h-full flex-col justify-end gap-5 p-8" intent="light">
       <div className="flex items-center justify-between">
         <div className="rounded-xl h-25 w-25 flex items-center justify-center overflow-hidden">
           {doc.previewImage ? (
@@ -35,7 +24,7 @@ export default function Doc(props: { doc: any }) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-3">
+      <div className="row-start-2 flex flex-col gap-3">
         <Heading level={4} size={5} className="transition-colors">
           {doc.title}
         </Heading>
