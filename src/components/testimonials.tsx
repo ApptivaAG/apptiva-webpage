b975b346abc060132db39af1afac017c5f12bc11
Patrik Stutz <patrik.stutz@gmail.com>
2026-03-19 15:14:34 +0100

    add testimonial schema

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,29 +1,64 @@
-import { getTestimonialsData } from '@/domain/testimonials'
+import {
+  getTestimonialsData,
+  Testimonial as TestimonialData,
+} from '@/domain/testimonials'
 import Testimonial from './testimonial'
 import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+import { Schema } from './schema'
 
 const Testimonials = async () => {
   const testimonials = await getTestimonialsData()
 
   return (
-    <div className="full">
-      <Carousel
-        loop={false}
-        layout={'autoWidth'}
-        numberOfSlides={testimonials.length}
-        className="py-14 md:py-16"
-      >
-        <CarouselContent className="gap-96">
-          {testimonials.map((testimonial, index) => {
-            return (
-              <CarouselItem key={index} index={index} className="basis-full">
-                <Testimonial testimonial={testimonial}></Testimonial>
-              </CarouselItem>
-            )
-          })}
-        </CarouselContent>
-      </Carousel>
-    </div>
+    <>
+      <TestimonialsSchema testimonials={testimonials} />
+      <div className="full">
+        <Carousel
+          loop={false}
+          layout={'autoWidth'}
+          numberOfSlides={testimonials.length}
+          className="py-14 md:py-16"
+        >
+          <CarouselContent className="gap-96">
+            {testimonials.map((testimonial, index) => {
+              return (
+                <CarouselItem key={index} index={index} className="basis-full">
+                  <Testimonial testimonial={testimonial}></Testimonial>
+                </CarouselItem>
+              )
+            })}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </>
+  )
+}
+
+const TestimonialsSchema = ({
+  testimonials,
+}: {
+  testimonials: TestimonialData[]
+}) => {
+  return (
+    <Schema
+      data={{
+        '@context': 'https://schema.org',
+        '@type': 'ItemList',
+        itemListElement: testimonials.map((testimonial) => ({
+          '@type': 'Review',
+          reviewBody: '',
+          author: {
+            '@type': 'Person',
+            name: testimonial.frontmatter.name,
+            jobTitle: testimonial.frontmatter.position,
+            worksFor: {
+              '@type': 'Organization',
+              name: testimonial.frontmatter.company,
+            },
+          },
+        })),
+      }}
+    />
   )
 }
 
