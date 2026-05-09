e810e8876b9896b1ef3d82fb39e3eee7088134de
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-05-09 09:36:11 +0200

    fix: snyk vulns

--- flake.nix
+++ flake.nix
@@ -1,10 +1,19 @@
 {
-  outputs = { self, nixpkgs,flake-utils }: flake-utils.lib.eachDefaultSystem (system: with nixpkgs.legacyPackages.${system}; {
-    devShells.default = mkShell {
-      buildInputs = [
-        nodejs
-        pnpm
-      ];
-    };
-  });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system: with nixpkgs.legacyPackages.${system}; {
+        devShells.default = mkShell {
+          buildInputs = [
+            nodejs
+            pnpm
+            snyk
+          ];
+        };
+      }
+    );
 }
