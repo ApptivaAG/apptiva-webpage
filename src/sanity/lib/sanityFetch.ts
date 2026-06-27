c580a97ee54be32f0551fffbc650f01ec075e94e
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-06-27 15:39:39 +0200

    test: added logging to sanity queries

--- src/sanity/lib/sanityFetch.ts
+++ src/sanity/lib/sanityFetch.ts
@@ -12,6 +12,7 @@ export const runQuery = makeSafeQueryRunner(
     params: Record<string, number | string> = {},
     tags?: string[]
   ) => {
+    console.log('sanity runQuery: ', query, params, tags)
     return client
       .withConfig({
         token: token,
@@ -30,6 +31,7 @@ export async function load<T extends GroqdQuery>(
   params: Record<string, number | string> = {},
   cacheTags?: string[]
 ) {
+  console.log('sanity load: ', query, params, cacheTags)
   const result = await loadQuery<InferType<T>>(query.query, params, {
     next: { tags: cacheTags },
   })
