f7a20a07638db06764c19587eca186bcb9a565dc
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-07-20 16:35:48 +0200

    feat: moved redirects to own file

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from 'next'
+import redirects from './redirects'
+
+const withMDX = require('@next/mdx')
+const { withPlausibleProxy } = require('next-plausible')
+
+const config: NextConfig = {
+  output: 'standalone',
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  trailingSlash: false,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '**',
+      },
+    ],
+  },
+  experimental: {
+    taint: true,
+  },
+  redirects: async () => redirects,
+}
+
+export default withPlausibleProxy()(withMDX()(config))
