9d04d9b71cc577f1d54251a373b80ec16d981770
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-09-15 10:58:43 +0200

    feat: use type from same package

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,5 +1,4 @@
-import { SanityImageSource } from '@sanity/asset-utils'
-import { createImageUrlBuilder } from '@sanity/image-url'
+import { createImageUrlBuilder, SanityImageSource } from '@sanity/image-url'
 import { dataset, projectId } from '../env'
 
 const imageBuilder = createImageUrlBuilder({
